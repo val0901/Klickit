e68ce180fe254dcc0314d492c67201bf14ef956c
Shifu33 <appeliustheo@gmail.com>
2016-12-12 17:58:56 +0100

    update

--- app/Views/back/Admin/addAdmin.php
+++
@@ -1,5 +0,0 @@
-<?php $this->layout('layoutback', ['title' => 'Ajouter un administrateur']) ?>
-
-<?php $this->start('main_content') ?>
-	
-<?php $this->stop('main_content') ?>
--- app/Views/back/Admin/updateAdmin.php
+++
@@ -1,5 +0,0 @@
-<?php $this->layout('layoutback', ['title' => 'Mettre à jour un administrateur']) ?>
-
-<?php $this->start('main_content') ?>
-	
-<?php $this->stop('main_content') ?>
