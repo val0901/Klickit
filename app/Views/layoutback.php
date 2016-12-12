f833d789fe46b2760701515a9691c3250c32ec28
Shifu33 <appeliustheo@gmail.com>
2016-12-12 17:29:28 +0100

    Jquery Back

--- app/Views/layoutback.php
+++ app/Views/layoutback.php
@@ -63,6 +63,11 @@
 				</div>
 
 				<footer>
+				<script
+			  src="https://code.jquery.com/jquery-3.1.1.min.js"
+			  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+			  crossorigin="anonymous"></script>
+			  	<?= $this->section('js') ?>
 				</footer>
 			</div>
 		</body>
