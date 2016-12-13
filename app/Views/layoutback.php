6c7baddf0d2e0067f6e3bacbf2d65ca063755ff4
val0901 <symezac.valerie@neuf.fr>
2016-12-13 12:29:37 +0100

    ajout font awesome back

--- app/Views/layoutback.php
+++ app/Views/layoutback.php
@@ -7,6 +7,7 @@
 			<link href="https://fonts.googleapis.com/css?family=Advent+Pro|Roboto:400,700" rel="stylesheet">
 
 			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+			<link rel="stylesheet" href="<?= $this->assetUrl('css/font-awesome.min.css') ?>">
 			<link rel="stylesheet" href="<?= $this->assetUrl('css/styleback.css') ?>">
 			<link rel="stylesheet" href="<?= $this->assetUrl('css/jquery-confirm.min.css') ?>">
 		</head>
