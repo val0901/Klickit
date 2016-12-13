9c08394f8059a266d50cb425e5fef774bbed17a1
dankdoggo <duhfanofdoge@gmail.com>
2016-12-13 10:43:18 +0100

    updateUser + MAJ UserController (pour l'update)

--- app/Controller/UserController.php
+++ app/Controller/UserController.php
@@ -126,10 +126,109 @@ class UserController extends Controller
 
 	/**
 	 * Mise à jour des utilisateurs
+	 * @param int $id l'id du membre qu'on veut modifier
 	 */
-	public function updateUser()
-	{
-		$this->show('back/User/updateUser');
+	public function updateUser($id)
+	{	
+		
+		//Sert pour afficher les infos de l'utilisateur
+		if(!is_numeric($id) || empty($id)){
+			$this->showNotFound();
+		}else{
+			$update  = new UserModel();
+			$user = $update->find($id);
+
+		}
+
+		//Sert pour contrôler les entrées du formulaire
+		$post = [];
+		$errors = [];
+		$social_title = ['M', 'Mme'];
+		$role = ['Admin', 'Utilisateur'];
+		$hashPassword = new AuthentificationModel();
+		$insert = new UserModel();
+		$success = false;
+
+		if(!empty($_POST)){
+			
+			//On nettoie les entrées du formulaire
+			foreach ($_POST as $key => $value) {
+				$post[$key] = trim(strip_tags($value));
+			}
+
+			//Contrôle des entrées du formulaire
+
+			if(!in_array($post['social_title'], $social_title)){
+				$errors[] = 'Vous devez choisir votre civilité (M ou Mme)';
+			}
+
+			if(!in_array($post['role'], $role)){
+				$errors[] = 'Vous devez choisir un niveau d\'utilisateur (Admin ou Utilisateur)';
+			}
+
+			if(!v::notEmpty()->length(3,20)->validate($post['firstname'])){
+				$errors[] = 'Le prénom doit comporter entre 3 et 20 caractères';
+			}
+
+			if(!v::notEmpty()->length(3,20)->validate($post['lastname'])){
+				$errors[] = 'Le nom doit comporter entre 3 et 20 caractères';
+			}
+
+			if(!v::notEmpty()->length(3,20)->validate($post['username'])){
+				$errors[] = 'Le pseudonyme doit comporter entre 3 et 20 caractères';
+			}
+
+			if(!v::notEmpty()->length(5,null)->validate($post['address'])){
+				$errors[] = 'Votre adresse doit comporter au moins 5 caractères';
+			}
+
+			if(!v::notEmpty()->digit()->length(5,5)->validate($post['zipcode'])){
+				$errors[] = 'Votre adresse doit comporter au moins 5 caractères';
+			}
+
+			if(!v::notEmpty()->length(3,null)->validate($post['city'])){
+				$errors[] = 'Votre adresse doit comporter au moins 3 caractères';
+			}
+
+			if(!v::notEmpty()->email()->validate($post['email'])){
+				$errors[] = 'L\'adresse email n\'est pas valide';
+			}
+
+			if(!v::notEmpty()->length(8,20)->validate($post['password'])){
+				$errors[] = 'Votre mot de passe doit comporter entre 8 et 20 caractères';
+			}
+
+			if(count($errors) === 0){
+				
+				$dataUpdate = [
+					'social_title'	=> $post['social_title'],
+					'role'		=> $post['role'],
+					'firstname'	=> $post['firstname'],
+					'lastname'	=> $post['lastname'],
+					'username'	=> $post['username'],
+					'adress'	=> $post['address'],
+					'zipcode'	=> $post['zipcode'],
+					'city'	=> $post['city'],
+					'email'	=> $post['email'],
+					'password'	=> $hashPassword->hashPassword($post['password']),
+				];
+
+				if($insert->update($dataUpdate, $id)){
+					$success = true;
+				}else{
+					$errors[] = 'Erreur lors de l\'ajout en base de données';
+				}
+				
+			}
+
+		}
+		$data = [
+			'user'		=> $user, //Va récupérer les infos de l'utilisateur pour préremplir les champs
+			'success'	=> $success, //success et errors vont nous servir à afficher les messages d'erreur ou de réussite
+			'errors'	=> $errors
+		];	
+
+		$this->show('back/User/updateUser', $data);
 	}
 
 	/**
--- app/Views/back/User/updateUser.php
+++ app/Views/back/User/updateUser.php
@@ -1,5 +1,137 @@
 <?php $this->layout('layoutback', ['title' => 'Mettre à jour client']) ?>
 
 <?php $this->start('main_content') ?>
-	
+
+<?php if (empty($user)) :?>
+	<p class="alert alert-danger">Aucun utilisateur trouvé</p>
+<?php else :?>
+
+	<?php if(!empty($errors)):?>
+		<p class="alert alert-danger">
+			<?=implode('<br>',$errors);?>
+		</p>	
+	<?php elseif($success):?>
+		<p class="alert alert-success">Utilisateur mis à jour</p>
+	<?php endif;?>
+			
+	<form class="form-horizontal" method="post">
+
+		<!-- Form Name -->
+		<legend>Création d'un compte client</legend>
+
+		<!-- Select Basic -->
+		<div class="form-group">
+		  <label class="col-md-4 control-label" for="social_title">Civilité</label>
+		  <div class="col-md-1">
+		    <select id="social_title" name="social_title" class="form-control">
+		       	<?php if($user['social_title'] == 'M'):?>
+			      	<option value="M" selected>M.</option>
+			      	<option value="Mme">Mme</option>
+		       	<?php elseif($user['social_title'] == 'Mme'):?>
+		       		<option value="M">M.</option>
+			      	<option value="Mme" selected>Mme</option>
+			   	<?php endif;?>   	
+		    </select>
+		  </div>
+		</div>
+
+		<!-- Select Basic -->
+		<div class="form-group">
+		  <label class="col-md-4 control-label" for="role">Rôle</label>
+		  <div class="col-md-4">
+		    <select id="role" name="role" class="form-control">
+		      	<?php if($user['role'] == 'Admin'):?>	
+			      	<option value="Admin" selected>Admin</option>
+			      	<option value="Utilisateur">Utilisateur</option>
+			  	<?php elseif($user['role'] == 'Utilisateur'):?>
+			  	  	<option value="Admin">Admin</option>
+			      	<option value="Utilisateur" selected>Utilisateur</option>	
+			  	<?php endif;?>   
+		    </select>
+		  </div>
+		</div>
+
+		<!-- Text input-->
+		<div class="form-group">
+		  <label class="col-md-4 control-label" for="firstname">Prénom</label>  
+		  <div class="col-md-4">
+		  <input id="firstname" name="firstname" value="<?=$user['firstname'];?>" class="form-control input-md" required="" type="text">
+		    
+		  </div>
+		</div>
+
+		<!-- Text input-->
+		<div class="form-group">
+		  <label class="col-md-4 control-label" for="lastname">Nom</label>  
+		  <div class="col-md-4">
+		  <input id="lastname" name="lastname" value="<?=$user['lastname'];?>" class="form-control input-md" required="" type="text">
+		    
+		  </div>
+		</div>
+
+		<!-- Text input-->
+		<div class="form-group">
+		  <label class="col-md-4 control-label" for="username">Pseudonyme</label>  
+		  <div class="col-md-4">
+		  <input id="username" name="username" value="<?=$user['username'];?>" class="form-control input-md" required="" type="text">
+		    
+		  </div>
+		</div>
+
+		<!-- Text input-->
+		<div class="form-group">
+		  <label class="col-md-4 control-label" for="address">Adresse</label>  
+		  <div class="col-md-4">
+		  <input id="address" name="address" value="<?=$user['adress'];?>" class="form-control input-md" required="" type="text">
+		    
+		  </div>
+		</div>
+
+		<!-- Text input-->
+		<div class="form-group">
+		  <label class="col-md-4 control-label" for="zipcode">Code postal</label>  
+		  <div class="col-md-4">
+		  <input id="zipcode" name="zipcode" value="<?=$user['zipcode'];?>" class="form-control input-md" required="" type="text">
+		    
+		  </div>
+		</div>
+
+		<!-- Text input-->
+		<div class="form-group">
+		  <label class="col-md-4 control-label" for="city">Ville</label>  
+		  <div class="col-md-4">
+		  <input id="city" name="city" value="<?=$user['city'];?>" class="form-control input-md" required="" type="text">
+		    
+		  </div>
+		</div>
+
+		<!-- Text input-->
+		<div class="form-group">
+		  <label class="col-md-4 control-label" for="email">Adresse email</label>  
+		  <div class="col-md-4">
+		  <input id="email" name="email" value="<?=$user['email'];?>" class="form-control input-md" required="" type="text">
+		    
+		  </div>
+		</div>
+
+		<!-- Password input-->
+		<div class="form-group">
+		  <label class="col-md-4 control-label" for="password">Mot de passe</label>
+		  <div class="col-md-4">
+		    <input id="password" name="password" class="form-control input-md" required="" type="password">
+		    
+		  </div>
+		</div>
+
+		<!-- Button -->
+		<div class="form-group">
+		  <label class="col-md-4 control-label" for="id"></label>
+		  <div class="col-md-4">
+		    <button id="id" name="id" class="btn btn-info">Créer un compte client</button>
+		  </div>
+		</div>
+
+		</fieldset>
+	</form>
+<?php endif;?>	
 <?php $this->stop('main_content') ?>
