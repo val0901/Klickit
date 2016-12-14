b9bf15af0e0ebee1d9aad787bc75fb3cd22616a5
Shifu33 <appeliustheo@gmail.com>
2016-12-14 15:17:36 +0100

    Item Update

--- app/Controller/ItemController.php
+++ app/Controller/ItemController.php
@@ -210,6 +210,7 @@ class ItemController extends Controller
 			$afficheItem = $affiche->find($id);
 		}
 
+		$dataUpdate = [];
 		$post = [];
 		$errors = [];
 		$statut_product = ['promotion', 'nouveauté', 'par defaut'];
@@ -223,61 +224,93 @@ class ItemController extends Controller
 		if(!empty($_POST)){
 			$post = array_map('trim', array_map('strip_tags', $_POST));
 
-			if(!in_array($post['statut'], $statut_product)){
-				$errors[] = 'Veuillez choisir un statut valide';
+			if(!empty($post['statut']) && isset($post['statut'])) {
+				if(!in_array($post['statut'], $statut_product)){
+					$errors[] = 'Veuillez choisir un statut valide';
+				}
+				else {
+					$dataUpdate['statut'] = $post['statut'];
+				}
 			}
 
-			if(!in_array($post['category'], $category_product)){
-				$errors[] = 'Veuillez choisir une catégorie valide';
+			if(!empty($post['category']) && isset($post['category'])) {
+				if(!in_array($post['category'], $category_product)){
+					$errors[] = 'Veuillez choisir une catégorie valide';
+				}
+				else{
+					$dataUpdate['category'] = $post['category'];
+				}
 			}
 
-			if(!v::notEmpty()->length(3,30)->validate($post['name'])){
-				$errors[] = 'Le nom du produit doit comporter entre 3 et 30 caractères';
+			if(!empty($post['name']) && isset($post['name'])) {
+				if(!v::notEmpty()->length(3,30)->validate($post['name'])){
+					$errors[] = 'Le nom du produit doit comporter entre 3 et 30 caractères';
+				}
+				else {
+					$dataUpdate['name'] = $post['name'];
+				}
 			}
 
-			if(!v::notEmpty()->length(5,null)->validate($post['description'])){
-				$errors[] = 'La description doit comporter au moins 5 caractères';
+			if(!empty($post['description']) && isset($post['description'])) {
+				if(!v::notEmpty()->length(5,null)->validate($post['description'])){
+					$errors[] = 'La description doit comporter au moins 5 caractères';
+				}
+				else {
+					$dataUpdate['description'] = $post['description'];
+				}
 			}
 
-			if(!v::notEmpty()->digit()->length(1,null)->validate($post['quantity'])){
-				$errors[] = 'La quantité doit comporter au moins 1 unité';
+			if(!empty($post['quantity']) && isset($post['quantity'])) {
+				if(!v::notEmpty()->digit()->length(1,null)->validate($post['quantity'])){
+					$errors[] = 'La quantité doit comporter au moins 1 unité';
+				}
+				else {
+					$dataUpdate['quantity'] = $post['quantity'];
+				}
 			}
 
-			if(!v::notEmpty()->digit()->length(1,null)->validate($post['newPrice'])){
-				$errors[] = 'Le nouveau prix doit être supérieur à 0';
+			if(!empty($post['newPrice']) && isset($post['newPrice'])) {
+				if(!v::notEmpty()->digit()->length(1,null)->validate($post['newPrice'])){
+					$errors[] = 'Le nouveau prix doit être supérieur à 0';
+				}
+				else {
+					$dataUpdate['newPrice'] = $post['newPrice'];
+				}
 			}
 
+			if(!empty($post['picture1']) && isset($post['picture1'])) {
+				if(!v::image()->validate($_FILES['picture1']['tmp_name'])){
+					$errors[] = 'Le fichier envoyé dans "Image 1" n\'est pas une image valide';
+				}
 
-			if(!v::image()->validate($_FILES['picture1']['tmp_name'])){
-				$errors[] = 'Le fichier envoyé dans "Image 1" n\'est pas une image valide';
+				if(!v::size(null, '6MB')->validate($_FILES['picture1']['tmp_name'])){
+					$errors[] = 'La taille de votre image dans "Image 1" doit être inférieur à 2MB';
+				}
+
+				if(!v::uploaded()->validate($_FILES['picture1']['tmp_name'])){
+					$errors[] = 'Une erreur est survenue lors de l\'upload de l\'image dans "Image 1"';
+				}
 			}
 
-			if(!v::size(null, '10MB')->validate($_FILES['picture1']['tmp_name'])){
-				$errors[] = 'La taille de votre image dans "Image 1" doit être inférieur à 2MB';
-			}
+			if(!empty($post['picture2']) && isset($post['picture2'])) {
+				if(!v::image()->validate($_FILES['picture2']['tmp_name'])){
+					$errors[] = 'Le fichier envoyé dans "Image 1" n\'est pas une image valide';
+				}
 
-			if(!v::uploaded()->validate($_FILES['picture1']['tmp_name'])){
-				$errors[] = 'Une erreur est survenue lors de l\'upload de l\'image dans "Image 1"';
-			}
+				if(!v::size(null, '6MB')->validate($_FILES['picture2']['tmp_name'])){
+					$errors[] = 'La taille de votre image dans "Image 1" doit être inférieur à 2MB';
+				}
 
-
-			if(!v::image()->validate($_FILES['picture2']['tmp_name'])){
-				$errors[] = 'Le fichier envoyé dans "Image 1" n\'est pas une image valide';
-			}
-
-			if(!v::size(null, '10MB')->validate($_FILES['picture2']['tmp_name'])){
-				$errors[] = 'La taille de votre image dans "Image 1" doit être inférieur à 2MB';
-			}
-
-			if(!v::uploaded()->validate($_FILES['picture2']['tmp_name'])){
-				$errors[] = 'Une erreur est survenue lors de l\'upload de l\'image dans "Image 1"';
+				if(!v::uploaded()->validate($_FILES['picture2']['tmp_name'])){
+					$errors[] = 'Une erreur est survenue lors de l\'upload de l\'image dans "Image 1"';
+				}
 			}
 
 
 			if(count($errors) === 0) {
-				$img1 = Image::make($_FILES['picture1']['tmp_name']);
-				$img2 = Image::make($_FILES['picture2']['tmp_name']);
 
+			if(!empty($post['picture1']) && isset($post['picture1'])) {	
+				$img1 = Image::make($_FILES['picture1']['tmp_name']);
 				switch($img1->mime()){
 					case 'image/jpg':
 					case 'image/jpeg':
@@ -290,7 +323,14 @@ class ItemController extends Controller
 						$extension = '.gif';
 					break;
 				}
+				$imgName = uniqid('art_').$extension;
+				if($img1->save($fullFolderUpload.$imgName)){
+					$dataUpdate['picture1'] = $imgName;
+				}
+			}
 
+			if(!empty($post['picture2']) && isset($post['picture2'])) {
+				$img2 = Image::make($_FILES['picture2']['tmp_name']);
 				switch($img2->mime()){
 					case 'image/jpg':
 					case 'image/jpeg':
@@ -302,29 +342,14 @@ class ItemController extends Controller
 					case 'image/gif':
 						$extension2 = '.gif';
 					break;
-
 				}
-
-				$imgName = uniqid('art_').$extension;
 				$imgName2 = uniqid('art_').$extension2;
-
-				$img1->save($fullFolderUpload.$imgName);
-				$img2->save($fullFolderUpload.$imgName2);
-
-				$itemModel = new ItemModel();
-
-				$insert = $itemModel->insert([
-					'name'		  => $post['name'],
-					'description' => $post['description'],
-					'quantity' 	  => $post['quantity'],
-					'newPrice' 	  => $post['newPrice'],
-					'picture1'	  => $imgName,
-					'picture2'    => $imgName2,
-					'statut' 	  => $post['statut'],
-					'category' 	  => $post['category'],
-				]);
-
-				if($insert){
+				if($img2->save($fullFolderUpload.$imgName2)){
+					$dataUpdate['picture2'] = $imgName2;
+				}
+			}
+			
+				if($insert->update($dataUpdate, $id)){
 					$success = true;
 				}
 				else {
