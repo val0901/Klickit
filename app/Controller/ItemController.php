c70d219006c89a2174201b2622071a3654473c19
Shifu33 <appeliustheo@gmail.com>
2016-12-14 13:12:17 +0100

    relou relou relou
    Faudra peut-être faire un point sur tout les Update

--- app/Controller/ItemController.php
+++ app/Controller/ItemController.php
@@ -203,24 +203,152 @@ class ItemController extends Controller
 	 */
 	public function updateItem($id)
 	{	
-		$affiche = new ItemModel();
+		if(!is_numeric($id) || empty($id)){
+			$this->showNotFound();
+		}else{
+			$affiche = new ItemModel();
+			$afficheItem = $affiche->find($id);
+		}
 
-		$afficheItem = $affiche->find($id);
+		$post = [];
+		$errors = [];
+		$statut_product = ['promotion', 'nouveauté', 'par defaut'];
+		$category_product = ['PlaymobilClassique', 'PlaymobilCustom', 'PiecesDetachees', 'Divers'];
+		$insert = new ItemModel();
+		$success = false;
 
-		$data = [
-			'affichage' => $afficheItem,
-		];
+		$folderUpload = getApp()->getConfig('upload_dir'); 
+		$fullFolderUpload = $_SERVER['DOCUMENT_ROOT'].$_SERVER['W_BASE'].'/assets'.$folderUpload;
 
-		if(!empty($_SESSION)){
+		if(!empty($_POST)){
+			$post = array_map('trim', array_map('strip_tags', $_POST));
 
-			$this->show('back/Item/updateItem', $data);
-
-			if($_SESSION['role'] == 'Utilisateur') {
-				$this->redirectToRoute('front_index');
+			if(!in_array($post['statut'], $statut_product)){
+				$errors[] = 'Veuillez choisir un statut valide';
 			}
+
+			if(!in_array($post['category'], $category_product)){
+				$errors[] = 'Veuillez choisir une catégorie valide';
+			}
+
+			if(!v::notEmpty()->length(3,30)->validate($post['name'])){
+				$errors[] = 'Le nom du produit doit comporter entre 3 et 30 caractères';
+			}
+
+			if(!v::notEmpty()->length(5,null)->validate($post['description'])){
+				$errors[] = 'La description doit comporter au moins 5 caractères';
+			}
+
+			if(!v::notEmpty()->digit()->length(1,null)->validate($post['quantity'])){
+				$errors[] = 'La quantité doit comporter au moins 1 unité';
+			}
+
+			if(!v::notEmpty()->digit()->length(1,null)->validate($post['newPrice'])){
+				$errors[] = 'Le nouveau prix doit être supérieur à 0';
+			}
+
+
+			if(!v::image()->validate($_FILES['picture1']['tmp_name'])){
+				$errors[] = 'Le fichier envoyé dans "Image 1" n\'est pas une image valide';
+			}
+
+			if(!v::size(null, '10MB')->validate($_FILES['picture1']['tmp_name'])){
+				$errors[] = 'La taille de votre image dans "Image 1" doit être inférieur à 2MB';
+			}
+
+			if(!v::uploaded()->validate($_FILES['picture1']['tmp_name'])){
+				$errors[] = 'Une erreur est survenue lors de l\'upload de l\'image dans "Image 1"';
+			}
+
+
+			if(!v::image()->validate($_FILES['picture2']['tmp_name'])){
+				$errors[] = 'Le fichier envoyé dans "Image 1" n\'est pas une image valide';
+			}
+
+			if(!v::size(null, '10MB')->validate($_FILES['picture2']['tmp_name'])){
+				$errors[] = 'La taille de votre image dans "Image 1" doit être inférieur à 2MB';
+			}
+
+			if(!v::uploaded()->validate($_FILES['picture2']['tmp_name'])){
+				$errors[] = 'Une erreur est survenue lors de l\'upload de l\'image dans "Image 1"';
+			}
+
+
+			if(count($errors) === 0) {
+				$img1 = Image::make($_FILES['picture1']['tmp_name']);
+				$img2 = Image::make($_FILES['picture2']['tmp_name']);
+
+				switch($img1->mime()){
+					case 'image/jpg':
+					case 'image/jpeg':
+						$extension = '.jpg';
+					break;
+					case 'image/png':
+						$extension = '.png';
+					break;
+					case 'image/gif':
+						$extension = '.gif';
+					break;
+				}
+
+				switch($img2->mime()){
+					case 'image/jpg':
+					case 'image/jpeg':
+						$extension2 = '.jpg';
+					break;
+					case 'image/png':
+						$extension2 = '.png';
+					break;
+					case 'image/gif':
+						$extension2 = '.gif';
+					break;
+
+				}
+
+				$imgName = uniqid('art_').$extension;
+				$imgName2 = uniqid('art_').$extension2;
+
+				$img1->save($fullFolderUpload.$imgName);
+				$img2->save($fullFolderUpload.$imgName2);
+
+				$itemModel = new ItemModel();
+
+				$insert = $itemModel->insert([
+					'name'		  => $post['name'],
+					'description' => $post['description'],
+					'quantity' 	  => $post['quantity'],
+					'newPrice' 	  => $post['newPrice'],
+					'picture1'	  => $imgName,
+					'picture2'    => $imgName2,
+					'statut' 	  => $post['statut'],
+					'category' 	  => $post['category'],
+				]);
+
+				if($insert){
+					$success = true;
+				}
+				else {
+					$errors[] = 'Erreur lors de l\'ajout en base de données';
+				}
+			}
+
 		}
-		else {
-			$this->redirectToRoute('back_login');
-		}
+			$data = [
+				'affichage' => $afficheItem,
+				'success'	=> $success,
+				'errors'	=> $errors
+			];
+
+			if(!empty($_SESSION)){
+
+				$this->show('back/Item/updateItem', $data);
+
+				if($_SESSION['role'] == 'Utilisateur') {
+					$this->redirectToRoute('front_index');
+				}
+			}
+			else {
+				$this->redirectToRoute('back_login');
+			}
 	}
 }
--- app/Views/back/Item/updateItem.php
+++ app/Views/back/Item/updateItem.php
@@ -3,16 +3,16 @@
 <?php $this->start('main_content') ?>
 	<a href="<?=$this->url('listItem');?>"><button class="btn btn-info">Retour liste article</button></a>
 
+	<div class="preview col-lg-6">
+		<div class="preview-pic tab-content row">
+			<div class="tab-pane active col-md-6" id="pic-1"><img src="<?=$affichage['picture1'];?>" /></div>
+			<div class="tab-pane active col-md-6" id="pic-2"><img src="<?=$affichage['picture2'];?>" /></div>
+		</div>
+	</div>
 	<div class="container">
 		<div class="card">
 			<div class="container-fliud">
 				<div class="wrapper row">
-					<div class="preview col-md-6">
-						<div class="preview-pic tab-content">
-						  <div class="tab-pane active" id="pic-1"><img src="<?=$affichage['picture1'];?>" /></div>
-						  <div class="tab-pane active" id="pic-2"><img src="<?=$affichage['picture2'];?>" /></div>
-						</div>
-					</div>
 					<div class="details col-md-6">
 						<h3 class="product-title"><?=$affichage['name'];?></h3>
 						
@@ -31,4 +31,98 @@
 			</div>
 		</div>
 	</div>
+	<br><br>
+		<?php if($success): ?>
+			<p class="alert alert-success">Produit modifié</p>
+		<?php elseif(isset($errors) && !empty($errors)):?>
+			<p class="alert alert-danger"><?=implode('<br>', $errors);?></p>	
+		<?php endif;?>
+	<br><br>
+	<form class="form-horizontal" method="post" enctype="multipart/form-data">
+		<!-- Text input-->
+		<div class="form-group">
+		  <label class="col-md-4 control-label" for="name">Nom du produit</label>  
+		  <div class="col-md-4">
+		  <input id="name" name="name" placeholder="" class="form-control input-md" type="text" value="<?=$affichage['name'];?>">
+		    
+		  </div>
+		</div>
+
+		<!-- Textarea -->
+		<div class="form-group">
+		  <label class="col-md-4 control-label" for="description">Description</label>
+		  <div class="col-md-4">                     
+		    <textarea class="form-control" id="description" name="description" value="<?=$affichage['description'];?>"></textarea>
+		  </div>
+		</div>
+
+		<!-- Text input-->
+		<div class="form-group">
+		  <label class="col-md-4 control-label" for="quantity">Quantité</label>  
+		  <div class="col-md-4">
+		  <input id="quantity" name="quantity" placeholder="" class="form-control input-md" type="text" value="<?=$affichage['quantity'];?>">
+		    
+		  </div>
+		</div>
+
+		<!-- Text input-->
+		<div class="form-group">
+		  <label class="col-md-4 control-label" for="newPrice">Nouveau Prix</label>  
+		  <div class="col-md-4">
+		  <input id="newPrice" name="newPprice" placeholder="" class="form-control input-md" type="text" value="<?=$affichage['newPrice'];?>">
+		    
+		  </div>
+		</div>
+
+		<!-- File Button --> 
+		<div class="form-group">
+		  <label class="col-md-4 control-label" for="picture1">Image 1</label>
+		  <div class="col-md-4">
+		    <input id="picture1" name="picture1" class="input-file" type="file">
+		  </div>
+		</div>
+
+		<!-- File Button --> 
+		<div class="form-group">
+		  <label class="col-md-4 control-label" for="picture2">Image 2</label>
+		  <div class="col-md-4">
+		    <input id="picture2" name="picture2" class="input-file" type="file">
+		  </div>
+		</div>
+
+		<!-- Select Basic -->
+		<div class="form-group">
+		  <label class="col-md-4 control-label" for="statut">Statut</label>
+		  <div class="col-md-4">
+		    <select id="statut" name="statut" class="form-control">
+		      <option value="<?=$affichage['statut'];?>" selected disabled></option>
+		      <option value="promotion">promotion</option>
+		      <option value="nouveauté">nouveauté</option>
+		      <option value="par defaut">par defaut</option>
+		    </select>
+		  </div>
+		</div>
+
+		<!-- Select Basic -->
+		<div class="form-group">
+		  <label class="col-md-4 control-label" for="category">Catégorie</label>
+		  <div class="col-md-4">
+		    <select id="category" name="category" class="form-control">
+		      <option value="<?=$affichage['category'];?>" selected disabled></option>
+		      <option value="PlaymobilClassique">Playmobil Classique</option>
+		      <option value="PlaymobilCustom">Playmobil Custom</option>
+		      <option value="PiecesDetachees">Pièces Detachées</option>
+		      <option value="Divers">Divers</option>
+		    </select>
+		  </div>
+		</div>
+
+		<!-- Button -->
+		<div class="form-group">
+		  <label class="col-md-4 control-label" for="submit"></label>
+		  <div class="col-md-4">
+		    <button id="submit" name="submit" class="btn btn-info">Modifier le produit</button>
+		  </div>
+		</div>
+	</form>
 <?php $this->stop('main_content') ?>
