ce948b192f761d3a8189ff90a91a7207459418b4
Shifu33 <appeliustheo@gmail.com>
2016-12-14 18:49:04 +0100

    Ajout slide 0.5
    à finir demain ... Pour moi, celui qui y touche se fera foudroyer par
    Zeus

--- app/Controller/SlideController.php
+++ app/Controller/SlideController.php
@@ -41,8 +41,90 @@ class SlideController extends Controller
 	 */
 	public function addSlide()
 	{
-		$this->show('back/Slide/addSlide');
+
+		$post = [];
+		$errors = [];
+		$insert = new SlideModel();
+		$success = false;
+
+		$folderUpload = getApp()->getConfig('upload_dir_slide'); 
+		$fullFolderUpload = $_SERVER['DOCUMENT_ROOT'].$_SERVER['W_BASE'].'/assets'.$folderUpload;
+
+		if(!empty($_POST)){
+			$post = array_map('trim', array_map('strip_tags', $_POST));
+
+			if(!v::notEmpty()->length(3,50)->validate($post['title'])){
+				$errors[] = 'Le titre du slide doit comporter plus de 3 caractères';
+			}
+
+			if(!v::image()->validate($_FILES['picture']['tmp_name'])){
+				$errors[] = 'L\'image envoyé n\'est pas une image valide';
+			}
+
+			if(!v::size(null, '2MB')->validate($_FILES['picture']['tmp_name'])){
+				$errors[] = 'La taille de votre image doit être inférieur à 2MB';
+			}
+
+			if(!v::uploaded()->validate($_FILES['picture']['tmp_name'])){
+				$errors[] = 'Une erreur est survenue lors de l\'upload de l\'image';
+			}
+
+			if(!v::notEmpty()->url()->validate($post['link'])){
+				$errors[] = 'L\'url du slide n\'est pas valide';
+			}
+
+			if(count($errors) === 0){
+				$img = Image::make($_FILES['picture']['tmp_name']);
+
+				switch($img->mime()){
+					case 'image/jpg':
+					case 'image/jpeg':
+						$extension = '.jpg';
+					break;
+					case 'image/png':
+						$extension = '.png';
+					break;
+					case 'image/gif':
+						$extension = '.gif';
+					break;
+				}
+
+				$imgName = uniqid('slide_').$extension;
+				$img->save($fullFolderUpload.$imgName);
+
+				$dataInsert = [
+					'title'   => $post['title'],
+					'picture' => $imgName,
+					'link'	  => $post['link'],
+				];
+
+				if($insert->insert($dataInsert)){
+					$success = true;
+				}
+				else {
+					$errors[] = 'Erreur lors de l\'ajout en base de données';
+				}
+			}
+		}
+
+		$params = [
+			'success'	=> $success,
+			'errors'	=> $errors
+		];
+
+		if(!empty($_SESSION)){
+
+			$this->show('back/Slide/addSlide', $params);
+
+			if($_SESSION['role'] == 'Utilisateur') {
+				$this->redirectToRoute('front_index');
+			}
+		}
+		else {
+			$this->redirectToRoute('back_login');
+		}
 	}
+	
 
 	/**
 	 * Mise à jour de Slide
@@ -50,13 +132,5 @@ class SlideController extends Controller
 	public function updateSlide()
 	{
 		$this->show('back/Slide/updateSlide');
-	}
-
-	/** 
-	 * Suppression de Slide
-	 */
-	public function deleteSlide()
-	{
-		$this->show('back/Slide/deleteSlide');
 	}
 }
--- app/Views/back/Slide/addSlide.php
+++ app/Views/back/Slide/addSlide.php
@@ -1,5 +1,47 @@
 <?php $this->layout('layoutback', ['title' => 'Ajouter des slides']) ?>
 
 <?php $this->start('main_content') ?>
-	
+	<a href="<?=$this->url('listSlide');?>"><button class="btn btn-info">Retour à la liste des slides</button></a>
+	<br>
+	<?php if($success): ?>
+		<p class="alert alert-success">Slide créé</p>
+	<?php elseif(isset($errors) && !empty($errors)):?>
+		<p class="alert alert-danger"><?=implode('<br>', $errors);?></p>	
+	<?php endif;?>
+
+	<form class="form-horizontal" method="post" enctype="multipart/form-data">
+		<!-- Text input-->
+		<div class="form-group">
+		  <label class="col-md-4 control-label" for="Titre">Titre</label>  
+		  <div class="col-md-4">
+		  <input id="Titre" name="Titre" placeholder="" class="form-control input-md" type="text">
+		    
+		  </div>
+		</div>
+
+		<!-- File Button --> 
+		<div class="form-group">
+		  <label class="col-md-4 control-label" for="picture">Image</label>
+		  <div class="col-md-4">
+		    <input id="picture" name="picture" class="input-file" type="file">
+		  </div>
+		</div>
+
+		<!-- File Button --> 
+		<div class="form-group">
+		  <label class="col-md-4 control-label" for="link">Url de l'évènement lié</label>
+		  <div class="col-md-4">
+		    <input id="link" name="link" class="input-file" type="text">
+		  </div>
+		</div>
+
+		<!-- Button -->
+		<div class="form-group">
+		  <label class="col-md-4 control-label" for="submit"></label>
+		  <div class="col-md-4">
+		    <button id="submit" name="submit" class="btn btn-info">Enregistrer le Slide</button>
+		  </div>
+		</div>
+	</form>
+
 <?php $this->stop('main_content') ?>
