354c5aaabd7bbb8745ffa15114a8001afba0a0ce
cec33 <cecilelafont@emaginetapub.com>
2016-12-14 16:19:37 +0100

    page event

--- app/Views/front/Event/viewEvent.php
+++ app/Views/front/Event/viewEvent.php
@@ -11,6 +11,7 @@
 </div>
 <div class="event_shareRS">
     <!--https://developers.facebook.com/docs/plugins/share-button/#configurator-->
-    <iframe class="event_shareFB" src="https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&layout=button&size=large&mobile_iframe=true&width=88&height=28&appId" width="88" height="28" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+    <iframe class="event_shareFB" src="https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&layout=button&size=large&mobile_iframe=true&width=88&height=28&appId" width="88" height="28" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe> <iframe class="event_shareTW" src="https://platform.twitter.com/widgets/tweet_button.html?size=l&url=https%3A%2F%2Fdev.twitter.com%2Fweb%2Ftweet-button&via=twitterdev&related=twitterapi%2Ctwitter&text=custom%20share%20text&hashtags=example%2Cdemo" width="88" height="28" style="border:none;overflow:hidden" scrolling="no" title="Twitter Tweet Button" style="border: 0; overflow: hidden;"></iframe>
+
 </div>
 <?php $this->stop('main_content') ?>
