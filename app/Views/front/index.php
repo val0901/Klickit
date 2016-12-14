f210a2424135c8ffe9af9c4f58ad90023b26e8ca
dankdoggo <duhfanofdoge@gmail.com>
2016-12-14 18:38:17 +0100

    such comment, very display

--- app/Views/front/index.php
+++ app/Views/front/index.php
@@ -34,7 +34,7 @@
     <?php foreach($comments as $value) :?>
         <?php if($value['published'] == 'oui'):?>
         	<img class="img-responsive" src="<?=$this->assetUrl('/img/avatarBoy.png');?>" style="float: left;">
-        	  <span class="col2commtaire_margin commentaire_title">J'adore trop vos customs<span style="float:right;" class="quote_hide"><i class="fa fa-quote-right fa-4x" aria-hidden="true" style="color: #7bc942;"></i></span><li><span style="padding-left:50px;font-size:25px;font-weight:400;"><?=$value['content'].' '?></span><span style="font-size:25px;font-weight:400;"><?=' ...'.$value['firstname'].' '.ucfirst(substr($value['lastname'],0,1))?></span></li>
+        	  <span class="col2commtaire_margin commentaire_title"><?=$value['subject']?><span style="float:right;" class="quote_hide"><i class="fa fa-quote-right fa-4x" aria-hidden="true" style="color: #7bc942;"></i></span><li><span style="padding-left:50px;font-size:25px;font-weight:400;"><?=$value['content'].' '?></span><span style="font-size:25px;font-weight:400;"><?=' ...'.$value['firstname'].' '.ucfirst(substr($value['lastname'],0,1))?></span></li>
         	  </span>
         <?php endif;?>          
     <?php endforeach;?>      
