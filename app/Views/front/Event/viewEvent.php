f7fab277a9a1294b2dca9c90cf540f663537eae5
cec33 <cecilelafont@emaginetapub.com>
2016-12-14 14:56:49 +0100

    page event + img large

--- app/Views/front/Event/viewEvent.php
+++ app/Views/front/Event/viewEvent.php
@@ -2,11 +2,15 @@
 
 <?php $this->start('main_content') ?>
 
- <div class="event_title">
+ <div class="event_etitle">
      évenements
 </div>
-<div class="EVT_bgcolor">
+<div class="event_poster">
 	<img class="img-responsive" src="<?=$this->assetUrl('/events/expo_2016_noirmoutier.jpg');?>">
 
+</div>
+<div class="event_shareRS">
+    <!--https://developers.facebook.com/docs/plugins/share-button/#configurator-->
+    <iframe class="event_shareFB" src="https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&layout=button&size=large&mobile_iframe=true&width=88&height=28&appId" width="88" height="28" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
 </div>
 <?php $this->stop('main_content') ?>
