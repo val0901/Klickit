f2f96fb341edba7f8e698eeaf2e2a1ff08d51aa5
cec33 <cecilelafont@emaginetapub.com>
2016-12-15 12:03:30 +0100

    meta desc et title

--- app/Views/front/index.php
+++ app/Views/front/index.php
@@ -1,4 +1,4 @@
-<?php $this->layout('layoutfront', ['title' => 'Accueil']) ?>
+<?php $this->layout('layoutfront', ['title' => 'Playmobil Napoléon Western Pirate Fée Princesses Police Robots Espace Animaux Sport Tampographie Customs Accessoires Stickers Résine']) ?>
 
 <?php $this->start('main_content') ?>
 <!--section 4 categories-->
