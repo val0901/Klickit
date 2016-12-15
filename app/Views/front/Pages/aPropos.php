0a97b428b1d23f30e9cb4894197aa29665ce5258
cec33 <cecilelafont@emaginetapub.com>
2016-12-15 20:31:26 +0100

    fin page apropos

--- app/Views/front/Pages/aPropos.php
+++ app/Views/front/Pages/aPropos.php
@@ -1,60 +1,67 @@
 <?php $this->layout('layoutfront', ['title' => 'catégorie Classics', 'meta' => 'Klickit, Custom Playmobil, Personnalisation Playmobil, ']) ?>
 
 <?php $this->start('main_content') ?>
+<div class="apropos_container">
 
-<h1>
-La boutique
-</h1>
+    <h1 class="apropos_title">
+    La boutique
+    </h1>
 
-<p><strong>©Klickit</strong> est une entreprise familiale, née d&#39;un passionné de Playmobil© et
+    <p class="apropos_text"><strong>©Klickit</strong> est une entreprise familiale, née d&#39;un passionné de Playmobil© et d&#39;histoire. Depuis plusieurs années, nous proposons à travers les expositions Playmobils copyright aux quatre coins de la France, de nombreux personnages issus des gammes Playmobil copyright, mais aussi des customs de personnages historiques réalisé par tampographie, créations de pièces en résine, peinture, stickers, pièces détachées, etc... </p>
 
-d&#39;histoire. Depuis plusieurs années, nous proposons à travers les expositions Playmobils copyright
+    <p class="apropos_text">Désormais, nous vous proposons tous ses produits en ligne !</p>
 
-aux quatre coins de la France, de nombreux personnages issus des gammes Playmobil copyright ,
+    <p class="apropos_text">Nous nous efforçons de vous proposer le plus large choix possible de personnages ainsi que des nouveautés régulières.</p>
 
-mais aussi des customs de personnages historiques réalisé par tampographie, créations de pièces en
+    <p class="apropos_text">Tous nos produits sont neufs ou d&#39;occasion proche du neuf.</p>
 
-résine, peinture, stickers, pièces détachées, etc... </p>
+    <i class="fa fa-info-circle fa-2x apropospicinfo" aria-hidden="true"></i> <h2 class="apropos_txtinfo">Livraison</h2>
 
-Désormais, nous vous proposons tous ses produits
+    <p class="apropos_text">Nous faisons notre maximum pour expédier les commandes en un minimum de temps, <strong>généralement 24 à 48h ouvré.</strong></p>
 
-en ligne !
+    <p class="apropos_text">Toutes nos expéditions pour la France sont traitées par La Poste. Les délais d&#39;acheminements sont donc ceux de La Poste : 24 à 48h pour une lettre suivie et 48h pour un colissimo.</p>
 
-Nous nous efforçons de vous proposer le plus large choix possible de personnages ainsi que des
+     <i class="fa fa-info-circle fa-2x apropospicinfo" aria-hidden="true"></i> <h2 class="apropos_txtinfo">Frais de port pour la France métropolitaine :</h2>
+    <ul class="apropos_ul apropos_text">
+        <li >1 à 3 personnages <i class="fa fa-arrow-right fa-1x apropos_flech" aria-hidden="true"></i> articles = 2,50€</li>
 
-nouveautés régulières.
+        <li>4 à 8 personnages <i class="fa fa-arrow-right fa-1x apropos_flech" aria-hidden="true"></i> articles = 3,90€</li>
 
-Tous nos produits sont neufs ou d&#39;occasion proche du neuf.
+        <li>au delà <i class="fa fa-arrow-right fa-1x apropos_flech" aria-hidden="true"></i> forfait colissimo 6.90€</li>
 
-Livraison
+        <li>pièces en résine ou customs peints <i class="fa fa-arrow-right fa-1x apropos_flech" aria-hidden="true"></i> forfait colissimo 6.90€</li>
+    </ul>
+    
+     <i class="fa fa-info-circle fa-2x apropospicinfo" aria-hidden="true"></i> <h2 class="apropos_txtinfo">Paiement</h2>
+    <br><br>
+    <table class="apropos_containpaie">
+        <tr class="apropos_picsortpaie">
+            <td class="apropos_td"><i class="fa fa-credit-card fa-4x apropos_sortpaie" aria-hidden="true"> </i></td>
+            <td class="apropos_td"><i class="fa fa-cc-paypal fa-4x apropos_sortpaie" aria-hidden="true"></i></td>
+            <td class="apropos_td"><i class="fa fa-address-card-o fa-4x apropos_sortpaie" aria-hidden="true"></i></td>
+            <td class="apropos_td"><i class="fa fa-exchange fa-4x apropos_sortpaie" aria-hidden="true"></i></td>
+        </tr>
+        <tr class="apropos_sortpaie">
+            <td class="apropos_td">CB</td>
+            <td class="apropos_td">Paypal</td>
+            <td class="apropos_td">Chèque</td>
+            <td class="apropos_td">Virement</td>
+        </tr>
 
-Nous faisons notre maximum pour expédier les commandes en un minimum de temps,
+<!--
+        <div class="apropos_sortpaie">
+           
+            Paiement par carte bancaire<br>
 
-généralement 24 à 48h ouvré.
+            Paypal<br>
 
-Toutes nos expéditions pour la France sont traitées par La Poste. Les délais d&#39;acheminements sont
+            
+            Par chèque (expédition à réception du paiement)<br>
 
-donc ceux de La Poste : 24 à 48h pour une lettre suivie et 48h pour un colissimo.
-
-Frais de port pour la France métropolitaine
-
-1 à 3 personnages/articles = 2,50€
-
-4 à 8 personnages/ articles = 3,90€
-
-au delà = forfait colissimo 6.90€
-
-pièces en résine ou customs peints = forfait colissimo 6.90€
-
-Paiement
-
-- Paiement par carte bancaire
-
-- Paypal
-
-- Par chèque (expédition à réception du paiement)
-
-- Par virement bancaire
-
-
+            
+            Par virement bancaire<br>
+        </div>
+-->
+    </table>
+</div>
 <?php $this->stop('main_content') ?>
