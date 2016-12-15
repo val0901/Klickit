a2b22a018f9ab1a5374954c27b68c37955bbc0ee
cec33 <cecilelafont@emaginetapub.com>
2016-12-15 17:21:52 +0100

    page apropos

--- app/Views/front/Pages/aPropos.php
+++ app/Views/front/Pages/aPropos.php
@@ -1,6 +1,60 @@
-<?php $this->layout('layoutfront', ['title' => 'A Propos de Klickit']) ?>
+<?php $this->layout('layoutfront', ['title' => 'catégorie Classics', 'meta' => 'Klickit, Custom Playmobil, Personnalisation Playmobil, ']) ?>
 
 <?php $this->start('main_content') ?>
+
+<h1>
+La boutique
+</h1>
+
+<p><strong>©Klickit</strong> est une entreprise familiale, née d&#39;un passionné de Playmobil© et
+
+d&#39;histoire. Depuis plusieurs années, nous proposons à travers les expositions Playmobils copyright
+
+aux quatre coins de la France, de nombreux personnages issus des gammes Playmobil copyright ,
+
+mais aussi des customs de personnages historiques réalisé par tampographie, créations de pièces en
+
+résine, peinture, stickers, pièces détachées, etc... </p>
+
+Désormais, nous vous proposons tous ses produits
+
+en ligne !
+
+Nous nous efforçons de vous proposer le plus large choix possible de personnages ainsi que des
+
+nouveautés régulières.
+
+Tous nos produits sont neufs ou d&#39;occasion proche du neuf.
+
+Livraison
+
+Nous faisons notre maximum pour expédier les commandes en un minimum de temps,
+
+généralement 24 à 48h ouvré.
+
+Toutes nos expéditions pour la France sont traitées par La Poste. Les délais d&#39;acheminements sont
+
+donc ceux de La Poste : 24 à 48h pour une lettre suivie et 48h pour un colissimo.
+
+Frais de port pour la France métropolitaine
+
+1 à 3 personnages/articles = 2,50€
+
+4 à 8 personnages/ articles = 3,90€
+
+au delà = forfait colissimo 6.90€
+
+pièces en résine ou customs peints = forfait colissimo 6.90€
+
+Paiement
+
+- Paiement par carte bancaire
+
+- Paypal
+
+- Par chèque (expédition à réception du paiement)
+
+- Par virement bancaire
 
 
 <?php $this->stop('main_content') ?>
