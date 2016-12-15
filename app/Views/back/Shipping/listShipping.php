0e1098a7b888bb41cd29d533dd7877c8319374c7
dankdoggo <duhfanofdoge@gmail.com>
2016-12-15 11:17:13 +0100

    update shipping en cours

--- app/Views/back/Shipping/listShipping.php
+++ app/Views/back/Shipping/listShipping.php
@@ -21,6 +21,7 @@
 							<td><?=$option['title'];?></td>
 							<td><?=$option['price'];?></td>
 							<td><?=nl2br($option['content']);?></td>
+							<td><a href="<?=$this->url('updateShipping')?>">Mettre à jour l'option d'envoi</a></td>
 							<td><button class="btn btn-danger delete-option" data-id="<?=$option['id']?>">Effacer l'option d'envoi</button></td>
 						</tr>	
 					<?php endforeach;?>
