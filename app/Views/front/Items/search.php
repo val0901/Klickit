c3dc5e416477a03723d7a3a260daf1e59acd9967
jaycee55 <jaycxu@sina.com>
2016-12-16 16:07:58 +0100

    update

--- app/Views/front/Items/search.php
+++ app/Views/front/Items/search.php
@@ -1,6 +1,100 @@
 <?php $this->layout('layoutfront', ['title' => 'Recherche']) ?>
 
 <?php $this->start('main_content') ?>
-
+<div class="container_general">
+	<div class="row">
+		<div class="col-md-3 col-xs-6 viewcategoryrow2col1_img">
+			<a href="#"><img src="<?=$this->assetUrl('/img/art_classic_divers_000004.jpg');?>" alt="" class="img-thumbnail"></a>
+			<div class="viewcategorycaption">
+				<h4>5 €</h4>
+				<p>Princess Playmo</p>
+				<div class="viewcategory_nouveau">nouveau !</div>
+            </div>
+			<div class="viewcategory_button">
+				<button type="button" class="btn btn-primary viewcategory_button_size ">ajouter au panier</button>
+			</div>
+		</div>
+		<div class="col-md-3 col-xs-6 viewcategoryrow2col1_img">
+			<a href="#"><img src="<?=$this->assetUrl('/img/art_classic_divers_000005.jpg');?>" alt="" class="img-thumbnail"></a>
+			<div class="viewcategorycaption">
+				<h4>15 €</h4>
+				<p>Princess Playmo</p>
+				<div class="viewcategory_nouveau">nouveau !</div>
+            </div>
+			<div class="viewcategory_button">
+				<button type="button" class="btn btn-primary viewcategory_button_size ">ajouter au panier</button>
+			</div>
+		</div>
+		<div class="col-md-3 col-xs-6 viewcategoryrow2col1_img">
+			<a href="#"><img src="<?=$this->assetUrl('/img/art_classic_divers_000006.jpg');?>" alt="" class="img-thumbnail"></a>
+			<div class="viewcategorycaption">
+				<h4><span class="viewcategoryprixpromo">8 € </span> <span class="viewcategoryprixdelete">10 €</span></h4>
+				<p>Princess Playmo</p>
+				<div class="viewcategory_promo">promo !</div>
+				<div class="viewcategory_button">
+					<button type="button" class="btn btn-primary viewcategory_button_size ">ajouter au panier</button>
+				</div>
+            </div>
+		</div>
+		<div class="col-md-3 col-xs-6 viewcategoryrow2col1_img">
+			<a href="#"><img src="<?=$this->assetUrl('/img/art_classic_divers_000007.jpg');?>" alt="" class="img-thumbnail"></a>
+			<div class="viewcategorycaption">
+				<h4>20 €</h4>
+				<p>Princess Playmo</p>
+				<div class="viewcategory_nouveau">nouveau !</div>
+			<div class="viewcategory_button">
+				<button type="button" class="btn btn-primary viewcategory_button_size ">ajouter au panier</button>
+			</div>
+            </div>
+		</div>
+	</div>
+	
+	<div class="row">
+		<div class="col-md-3 col-xs-6 viewcategoryrow2col1_img">
+			<a href="#"><img src="<?=$this->assetUrl('/img/art_classic_divers_000004.jpg');?>" alt="" class="img-thumbnail"></a>
+			<div class="viewcategorycaption">
+				<h4>5 €</h4>
+				<p>Princess Playmo</p>
+				<div class="viewcategory_nouveau">nouveau !</div>
+            </div>
+			<div class="viewcategory_button">
+				<button type="button" class="btn btn-primary viewcategory_button_size ">ajouter au panier</button>
+			</div>
+		</div>
+		<div class="col-md-3 col-xs-6 viewcategoryrow2col1_img">
+			<a href="#"><img src="<?=$this->assetUrl('/img/art_classic_divers_000005.jpg');?>" alt="" class="img-thumbnail"></a>
+			<div class="viewcategorycaption">
+				<h4>15 €</h4>
+				<p>Princess Playmo</p>
+				<div class="viewcategory_nouveau">nouveau !</div>
+            </div>
+			<div class="viewcategory_button">
+				<button type="button" class="btn btn-primary viewcategory_button_size ">ajouter au panier</button>
+			</div>
+		</div>
+		<div class="col-md-3 col-xs-6 viewcategoryrow2col1_img">
+			<a href="#"><img src="<?=$this->assetUrl('/img/art_classic_divers_000006.jpg');?>" alt="" class="img-thumbnail"></a>
+			<div class="viewcategorycaption">
+				<h4><span class="viewcategoryprixpromo">8 € </span> <span class="viewcategoryprixdelete">10 €</span></h4>
+				<p>Princess Playmo</p>
+				<div class="viewcategory_promo">promo !</div>
+				<div class="viewcategory_button">
+					<button type="button" class="btn btn-primary viewcategory_button_size ">ajouter au panier</button>
+				</div>
+            </div>
+		</div>
+		<div class="col-md-3 col-xs-6 viewcategoryrow2col1_img">
+			<a href="#"><img src="<?=$this->assetUrl('/img/art_classic_divers_000007.jpg');?>" alt="" class="img-thumbnail"></a>
+			<div class="viewcategorycaption">
+				<h4>20 €</h4>
+				<p>Princess Playmo</p>
+				<div class="viewcategory_nouveau">nouveau !</div>
+			<div class="viewcategory_button">
+				<button type="button" class="btn btn-primary viewcategory_button_size ">ajouter au panier</button>
+			</div>
+            </div>
+		</div>
+	</div>
+</div>
 
 <?php $this->stop('main_content') ?>
