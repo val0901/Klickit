6ff7d7fced0e901643d785c784263528a262869d
dankdoggo <duhfanofdoge@gmail.com>
2016-12-16 16:21:46 +0100

    j'en ai chié des bulles pour y arriver.....(que je suis poétique)

--- app/Controller/MessageController.php
+++ app/Controller/MessageController.php
@@ -5,6 +5,8 @@ namespace Controller;
 use \W\Controller\Controller;
 use \Model\MessageModel;
 use \W\Security\AuthorizationModel;
+use \Respect\Validation\Validator as v;
+use \PHPMailer;
 
 class MessageController extends Controller 
 {
@@ -45,7 +47,7 @@ class MessageController extends Controller
 	}
 
 	/**
-	 * Vue unique d'un message avec changement de statut
+	 * Vue unique d'un message avec changement de statut et possibilité de répondre
 	 */
 	public function viewMessage($id)
 	{	
@@ -61,7 +63,7 @@ class MessageController extends Controller
 		}
 
 		//Changement du statut du message
-		if(isset($_POST['read'])){
+		if(isset($_GET['read'])){
 
 			//On change le statut du message
 			$status = [
@@ -71,19 +73,85 @@ class MessageController extends Controller
 
 			$this->redirectToRoute('listMessage'); //et on redirige
 
-		}elseif(isset($_POST['not-read'])){
+		}elseif(isset($_GET['not-read'])){
 
 			$status = [
 				'statut' => 'Non lu'
 			];
 			$updated = $update_status->update($status,$id);
 
-			$this->redirectToRoute('listMessage');
+			$this->redirectToRoute('listMessage');	
+		}
+
+		/********** GESTION DE LA REPONSE AU MESSAGE **********/
+
+		$post = [];
+		$errors = [];
+		$success = false;
+
+		if(!is_numeric($id) || empty($id)){
+			$this->showNotFound();
+		}else{
+
+			$getMessage  = new MessageModel();
+			$viewMessage = $getMessage->findOneMessage($id);
+		}
+
+		if(!empty($_POST)){
+
+			foreach ($_POST as $key => $value) {
+				$post[$key] = trim(strip_tags($value));
+			}
+
+			if(!v::notEmpty()->email()->validate($post['email'])){
+				$errors[] = 'L\'adresse email n\'est pas valide';
+			}
+
+			if(!v::notEmpty()->length(5,null)->validate($post['subject'])){
+				$errors[] = 'Le sujet du message doit au moins comporter 5 caractères';
+			}
+
+			if(!v::notEmpty()->length(10,null)->validate($post['content'])){
+				$errors[] = 'Le message doit au moins comporter 10 caractères';
+			}
+
+			if(count($errors) === 0){
+				$sendMail = new PHPMailer;
+
+				$contentEmail = nl2br($post['content']);
+
+				$sendMail->isSMTP();                                      
+				$sendMail->Host = 'smtp.gmail.com';  									// Hôte du SMTP
+				$sendMail->SMTPAuth = true;                               				// SMTP Authentification
+				$sendMail->Username = 'duhfanofdoge@gmail.com';          				// SMTP username
+				$sendMail->Password = 'TheRevA7X';                    	 				// SMTP password
+				$sendMail->SMTPSecure = 'tls';                         					// Enable TLS encryption, `ssl` also accepted
+				$sendMail->Port = 587;                                					// TCP port to connect to
+				$sendMail->CharSet = 'UTF-8';
+
+				$sendMail->setFrom('klickit@playmobil.dank', 'Klickit');		  				//Expéditeur
+				$sendMail->addAddress($viewMessage['email'], $viewMessage['firstname'].' '.$viewMessage['lastname']); 	   	//Destinataire
+				$sendMail->addCC('a7x.tholomew.plague@hotmail.fr'); //Copie envoyer à l'adresse souhaitée du mail
+
+				$sendMail->Subject = $post['subject'];
+				$sendMail->Body    = $contentEmail; //On envoi le message éventuellement en HTML
+				$sendMail->AltBody = $contentEmail; //On envoi le message sans HTML
+
+				if(!$sendMail->send()){
+					$errors[] = 'Erreur lors de l\'envoi du mail';
+
+				}else{
+					$success = true;
+				}
+			}
+
 		}
 
 		//On envoie le tableau $data qui contiendra les infos d'un message et le changement de statut
 		$data = [
 			'message'	=> $viewMessage,
+			'errors'	=> implode('<br>',$errors),
+			'success'	=> $success,
 		];
 
 		if(!empty($_SESSION)){
@@ -98,14 +166,5 @@ class MessageController extends Controller
 			$this->redirectToRoute('back_login');
 		}
 	}
-
-	/**
-	 * Suppression de message
-	 */
-	public function deleteMessage()
-	{
-		$this->show('back/Message/deleteMessage');
-	}
-
 	
 }
--- app/Views/back/Message/viewMessage.php
+++ app/Views/back/Message/viewMessage.php
@@ -3,7 +3,7 @@
 <?php $this->start('main_content') ?>
 	<?php if(empty($message)):?>
 		<p class="alert alert-danger">Aucun message trouvé</p>
-	<?php else:?>
+	<?php else : ?>	
 
 		<h3>Sujet : <?=$message['subject']?></h3>
 		<h3>Contenu : </h3>
@@ -11,10 +11,67 @@
 			<?=nl2br($message['content'])?>	
 		</div>
 
-		<form method="post">
+		<form method="get">
 			<button class="btn btn-success" name="read">Marquer comme lu</button>
 			<button class="btn btn-warning" name="not-read">Marquer comme non-lu</button>
+			<button class="btn btn-primary" id="answer-show">Répondre</button>
 		</form>	
+
+		<!-- message de réussite ou d'erreur pour l'envoi de la réponse -->
+		<div class="message" style="margin-top:20px;">
+			<?php if($success):?>
+				<p class="alert alert-success">Votre message est envoyé</p>	
+			<?php elseif(!empty($errors)):?>
+				<p class="alert alert-danger"><?=$errors?></p>
+			<?php endif;?>	
+		</div>	
+
+		<form method="post" id="answer" class="form-horizontal" style="margin-top:10px;display:none;">
+
+		<h3> Répondre au message de <?=$message['firstname'].' '.$message['lastname']?></h3>
+
+			<div class="form-group">
+		  		<label class="col-md-4 control-label" for="email">Votre adresse email</label>  
+		  		<div class="col-md-4">
+		  			<input id="email" name="email" placeholder="Sujet du message" class="form-control input-md" required="" type="text"> 
+		  		</div>
+			</div>
+
+			<div class="form-group">
+		  		<label class="col-md-4 control-label" for="subject">Sujet du message</label>  
+		  		<div class="col-md-4">
+		  			<input id="subject" name="subject" placeholder="Sujet du message" class="form-control input-md" required="" type="text"> 
+		  		</div>
+			</div>
+
+			<div class="form-group">
+				<label class="col-md-4 control-label" for="content">Votre message</label>  
+				<div class="col-md-4">
+		 			<textarea class="form-control input-md" id="content" name="content" placeholder="Votre message" required=""></textarea>
+		  		</div>
+			</div>
+
+			<div class="form-group">
+		 		<label class="col-md-4 control-label" for="id"></label>
+		 	 	<div class="col-md-4">
+		    		<button id="send" name="send_mail" type="submit" class="btn btn-success">Envoyer le message</button>
+		  		</div>
+			</div>
+
+		</form>
 
 <?php endif;?>
 <?php $this->stop('main_content') ?>
+
+<?php $this->start('js')?>
+	<script>
+		$(document).ready(function(){
+
+			$('#answer-show').click(function(e){
+				e.preventDefault();
+				$('#answer').toggle('slow');
+			});
+			
+		});
+	</script>
+<?php $this->stop('js')?>
