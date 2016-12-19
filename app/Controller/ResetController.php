0d9f583c4f3bf34aee2bf73961b6347f24a77cd9
dankdoggo <duhfanofdoge@gmail.com>
2016-12-19 12:02:12 +0100

    ...

--- app/Controller/ResetController.php
+++ app/Controller/ResetController.php
@@ -19,7 +19,7 @@ class ResetController extends Controller
 			$verify_token = new ResetModel;
 			$getInfos = $verify_token->find($id);
 
-			if($getInfos['date_expire'] == date('Y-m-d H:i:s',strtotime($getInfos['date_expire']+'2 days')){
+			if($getInfos['date_expire'] == date('Y-m-d H:i:s',strtotime($getInfos['date_expire']+'2 days'))){
 				$error = 'Le jeton a expiré. Pour réinitialiser le mot de passe, veuillez cliquer <a href="'.$this->url('back_forgot_pwd').'">ici</a>';
 			}
 
