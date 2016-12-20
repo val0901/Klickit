14fa1b2895ea6634f193619c6ea580866d392161
cec33 <cecilelafont@emaginetapub.com>
2016-12-20 14:34:30 +0100

    responsive img commentaires

--- app/Views/front/index.php
+++ app/Views/front/index.php
@@ -37,7 +37,7 @@
   <div class="col-md-10 col2_commentaireback">
     <?php foreach($comments as $value) :?>
         <?php if($value['published'] == 'oui'):?>
-        	<img class="img-responsive" src="<?=$this->assetUrl('/img/avatarBoy.png');?>" style="float: left;">
+        	<img class="img-responsive-tete" src="<?=$this->assetUrl('/img/avatarBoylarge.png');?>" style="float: left;">
         	  <span class="col2commtaire_margin commentaire_title"><?=$value['subject']?><span style="float:right;" class="quote_hide"><i class="fa fa-quote-right fa-4x" aria-hidden="true" style="color: #7bc942;"></i></span><li><span style="padding-left:50px;font-size:25px;font-weight:400;"><?=$value['content'].' '?></span><span style="font-size:25px;font-weight:400;"><?=' ...'.$value['firstname'].' '.ucfirst(substr($value['lastname'],0,1))?></span></li>
         	  </span>
         <?php endif;?>          
