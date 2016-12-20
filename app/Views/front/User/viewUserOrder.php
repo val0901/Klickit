cb2504a9442b0ddbdf8c3540df2582a53a9877da
profilejiexu <jaycxu@sina.com>
2016-12-20 22:52:39 +0100

    update

--- app/Views/front/User/viewUserOrder.php
+++ app/Views/front/User/viewUserOrder.php
@@ -1,6 +1,88 @@
 <?php $this->layout('layoutfront', ['title' => 'Commande utilisateur']) ?>
 
 <?php $this->start('main_content') ?>
+<div class="listorder_back">
+ <div class="row">
+  <div class="col-md-3">
+  	<img class="img-responsive playmo_hide" src="<?=$this->assetUrl('/img/napoLeft.png');?>" id="categorycustoms_hover">
+  </div>
+  <div class="col-md-6 ordermidia_width">
+      <div class="listorder_contenu">
+          <div>
+              <div class="row">
+                  <div class="col-xs-6">
+                    <img src="<?=$this->assetUrl('/img/KLICKIT-logo-napoleon.png');?>">
+                    <p class="baseline_text">Créez l'histoire !</p>
+                  </div>
+                  <div class="col-xs-6">
+                      <p class="viewuserorder_title">FACTURE</p>      
+                  </div>      
+              </div>
+              <br><br>
+              <div class="row">
+                  <div class="col-xs-6">
+                      <p>Laurent Lafont</p>
+                      <p>1, résidence beau pré</p>
+                      <p>33650 Saucats</p>
+                      <p>Téléphone 06 11 82 17 71</p>
+                  </div>
+                  <div class="col-xs-6">
+                      <p><strong>DATE :</strong></p>
+                      <p><strong>No. FACTURE :</strong></p>
+                  </div>
+              </div>
+              <br>
+              <p>No. SERET: 753 966 464 00012</p>
+              <br><br>
+              <p><strong>Facturé a :</strong></p>
+          </div>
+          
+          <!--viewuserorder table-->
+          <div class="table-responsive">
+              <table class="table table-bordered">
+                  <thead style="background-color: #ccc;">
+                      <tr>
+                          <th>DESCRIPTION</th>
+                          <th>Prix U.</th>
+                          <th>Qté</th>
+                          <th>MONTANT</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      <tr>
+                          <td>dfdsf</td>
+                          <td>dsfdsf</td>
+                          <td>dsfdsf</td>
+                          <td>€</td>
+                      </tr>
+                      <tr>
+                          <td colspan="3" style="text-align:right;font-weight:600;"><stong>TOTAL :</stong></td>
+                          <td>€</td>
+                      </tr>
+                  </tbody>
+              </table>
+              
+              <div>
+                <p style="text-align:right;">TVA on applicable. art. 293 B du CGI</p>
+                <br>
+                <div>
+                    <p style="line-height: 10px;">Veuillez libellé votre chèque a l'ordre de Lafont Laurent.</p>
+                    <p style="line-height: 10px;">Pour toute question concement cette facture.  Merci de me contacter a contact@klickit.fr</p>
+                    <br><br>
+                    <p style="text-align:center;"><strong>MERCI DE VOTRE CONFIANCE !</strong></p>
+                </div>
+              </div>
+          </div>
+          <!--End viewuserorder table-->
+      </div>
+  </div>
+  <div class="col-md-3">
+  	<img class="img-responsive playmo_hide" src="<?=$this->assetUrl('/img/napoRight.png');?>" id="categorycustoms_hover" style="float:right;">
+  </div>
+ </div>
+ 
+ 
+</div>
 
 
 <?php $this->stop('main_content') ?>
