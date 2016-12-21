af214a9358f8f45f601363c77d243156bb6bfe1f
jaycee55 <jaycxu@sina.com>
2016-12-21 12:10:01 +0100

    update

--- app/Views/front/User/cptUser.php
+++ app/Views/front/User/cptUser.php
@@ -27,7 +27,6 @@
 			   <p class="cptuser_textlien">histori<span style="letter-spacing: 0.01em;">q</span>mes favoris</p> 
 		   </div>
 		   <div class="col-md-3">
-		   	<img class="img-responsive" src="<?=$this->assetUrl('/img/gestbookvign.jpg');?>" id="cptuser_hover" style="cursor: pointer"> 
 		   </div>
 	   </div>
 	     
