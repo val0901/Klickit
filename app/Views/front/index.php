710b3e572f80a908a26fd77a48a67dca50aa1231
Dankest doggo eva <a7x.tholomew.plague@hotmail.fr>
2017-01-05 16:56:31 +0100

    index.php pour le slider

--- app/Views/front/index.php
+++ app/Views/front/index.php
@@ -3,60 +3,62 @@
 <?php $this->start('main_content') ?>
 <!--section 4 categories-->
 <div class="">
-	<div class="row">
+    <div class="row">
         <a href="<?=$this->url('listItemClassicsFull');?>">
-    		<div class="col-lg-3 col-xs-6" style="position:relative;" id="categoryclassic_hover">
-    			<div class="classic_text"><span>Classics</span></div>
-    			<img class="img-responsive" src="<?=$this->assetUrl('/img/art_classic_divers_000001.jpg');?>" id="categoryclassic_hover">
-    			<div class="classic_text_show"><span style="font-weight: 600;">Classics</span></div>
-    		</div>
+            <div class="col-lg-3 col-xs-6" style="position:relative;" id="categoryclassic_hover">
+                <div class="classic_text"><span>Classics</span></div>
+                <img class="img-responsive" src="<?=$this->assetUrl('/img/art_classic_divers_000001.jpg');?>" id="categoryclassic_hover">
+                <div class="classic_text_show"><span style="font-weight: 600;">Classics</span></div>
+            </div>
         </a>
 
         <a href="<?=$this->url('listItemCustomFull');?>">
-    		<div class="col-lg-3 col-xs-6" id="categorycustoms_hover">
-    			<div class="customs_text"><span>Customs</span></div>
-    			<img class="img-responsive" src="<?=$this->assetUrl('/img/art_classic_divers_000002.jpg');?>" id="categorycustoms_hover">
-    			<div class="customs_text_show" style="font-weight: 600;">Customs</div>
-    		</div>
+            <div class="col-lg-3 col-xs-6" id="categorycustoms_hover">
+                <div class="customs_text"><span>Customs</span></div>
+                <img class="img-responsive" src="<?=$this->assetUrl('/img/art_classic_divers_000002.jpg');?>" id="categorycustoms_hover">
+                <div class="customs_text_show" style="font-weight: 600;">Customs</div>
+            </div>
         </a>
         
         <a href="<?=$this->url('listItemPiecesFull');?>">
-    		<div class="col-lg-3 col-xs-6" id="categorypieces_hover">
-    			<div class="pieces_text"><span>Pièces</span></div>
-    			<img class="img-responsive" src="<?=$this->assetUrl('/img/art_classic_divers_000003.jpg');?>" id="categorypieces_hover">
-    			<div class="pieces_text_show" style="font-weight: 600;">Pièces</div>
-    		</div>
+            <div class="col-lg-3 col-xs-6" id="categorypieces_hover">
+                <div class="pieces_text"><span>Pièces</span></div>
+                <img class="img-responsive" src="<?=$this->assetUrl('/img/art_classic_divers_000003.jpg');?>" id="categorypieces_hover">
+                <div class="pieces_text_show" style="font-weight: 600;">Pièces</div>
+            </div>
         </a>
 
         <a href="<?=$this->url('listItemDiversFull');?>">
-    		<div class="col-lg-3 col-xs-6" id="categorydivers_hover">
-    			<div class="divers_text"><span>Divers</span></div>
-    			<img class="img-responsive" src="<?=$this->assetUrl('/img/art_divers_bte000001.jpg');?>" id="categorydivers_hover">
-    			<div class="divers_text_show" style="font-weight: 600;">Divers</div>
-    		</div>
+            <div class="col-lg-3 col-xs-6" id="categorydivers_hover">
+                <div class="divers_text"><span>Divers</span></div>
+                <img class="img-responsive" src="<?=$this->assetUrl('/img/art_divers_bte000001.jpg');?>" id="categorydivers_hover">
+                <div class="divers_text_show" style="font-weight: 600;">Divers</div>
+            </div>
         </a>
-	</div>
+    </div>
 </div>
 <!--End section 4 categories-->
 
 <!--La derniere commentaire-->
 <div class="vignetteEvent_hide">
-	<a href="<?=$this->url('front_contact')?>"><img class="img-responsive" src="<?=$this->assetUrl('/img/vignetteEvent1.png');?>" id="vignette_hover" onmouseover="vignettehover();" onmouseout="vignetteout();"></a>
+    <a href="<?=$this->url('front_contact')?>"><img class="img-responsive" src="<?=$this->assetUrl('/img/vignetteEvent1.png');?>" id="vignette_hover" onmouseover="vignettehover();" onmouseout="vignetteout();"></a>
 </div>
 <div class="row commtaire_back">
   <div class="col-md-2"></div>
-  <div class="col-md-10 col2_commentaireback">
-    <?php foreach($comments as $value) :?>
-        <div class="my-slider">
-            <?php if($value['published'] == 'oui'):?>
-                <div class="commentaire" id="<?=$value['id']?>">    
-                	<img class="img-responsive-tete" src="<?=$this->assetUrl('/img/avatarBoylarge.png');?>" style="float: left;">
-                	  <span class="col2commtaire_margin commentaire_title"><?=$value['subject']?><span style="float:right;" class="quote_hide"><i class="fa fa-quote-right fa-4x" aria-hidden="true" style="color: #7bc942;"></i></span><li><span style="padding-left:50px;font-size:25px;font-weight:400;"><?=$value['content'].' '?></span><span style="font-size:25px;font-weight:400;"><?=' ...'.$value['firstname'].' '.ucfirst(substr($value['lastname'],0,1))?></span></li>
-                	  </span>
-                </div>      
-            <?php endif;?>
-        </div>    
-    <?php endforeach;?>      
+  <div class="col-md-10 col2_commentaireback my-slider">
+    <ul>
+        <?php foreach($comments as $value) :?>
+                <?php if($value['published'] == 'oui'):?>
+                    <li>  
+                        <div class="commentaire">  
+                            <img class="img-responsive-tete" src="<?=$this->assetUrl('/img/avatarBoylarge.png');?>" style="float: left;">
+                              <span class="col2commtaire_margin commentaire_title"><?=$value['subject']?><span style="float:right;" class="quote_hide"><i class="fa fa-quote-right fa-4x" aria-hidden="true" style="color: #7bc942;"></i></span><span style="padding-left:50px;font-size:25px;font-weight:400;"><?=$value['content'].' '?></span><span style="font-size:25px;font-weight:400;"><?=' ...'.$value['firstname'].' '.ucfirst(substr($value['lastname'],0,1))?></span>
+                              </span>
+                        </div>
+                    </li>      
+                <?php endif;?>   
+        <?php endforeach;?>  
+    </ul>    
   </div>
 </div>
 <!--End La derniere commentaire-->
@@ -67,7 +69,7 @@
 <!--
     ../../../app/Views/front/Event/viewEvent.php
 -->
-	<a href="<?=$this->url('front_events');?>"> <img class="img-responsive" src="<?=$this->assetUrl('/img/slideEvent1.jpg');?>"></a>
+    <a href="<?=$this->url('front_events');?>"> <img class="img-responsive" src="<?=$this->assetUrl('/img/slideEvent1.jpg');?>"></a>
 </div>
 <!--End La derniere evenement-->
 
