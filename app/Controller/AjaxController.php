82b0f7b9d4e1270eeff413facb93a6f72dc6cec8
Shifu33 <appeliustheo@gmail.com>
2017-01-16 16:31:59 +0100

    Avancé dans la recherche Back

--- app/Controller/AjaxController.php
+++ app/Controller/AjaxController.php
@@ -276,8 +276,8 @@ class AjaxController extends Controller
 					$viewSearch.='<td>'.$value['firstname'].'</td>';
 					$viewSearch.='<td>'.$value['username'].'</td>';
 					$viewSearch.='<td>'.$value['email'].'</td>';
-					$viewSearch.='<td><a href="<?=$this->url("front_affcptuser", ["id"=>'.$value['id'].']);?>" target="_blank"><i class="fa fa-search-plus fa-2x" aria-hidden="true"></a></td>';
-					$viewSearch.='<td><a href="<?=$this->url("updateUser", ["id"=>'.$value['id'].']);?>">Mettre à jour le profil</a></td>';
+					$viewSearch.='<td><a href="<?=$this->url(\'front_affcptuser\', [\'id\'=>'.$value['id'].']);?>" target="_blank"><i class="fa fa-search-plus fa-2x" aria-hidden="true"></a></td>';
+					$viewSearch.='<td><a href="<?=$this->url(\'updateUser\', [\'id\'=>'.$value['id'].']);?>">Mettre à jour le profil</a></td>';
 					$viewSearch.='<td><button class="btn btn-danger delete-user" data-id="'.$value['id'].'">Effacer le profil</button></td>';
 				}
 			}
