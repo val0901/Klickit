fb68f6324b974fe228fc0b3bf90264ef279a4f8f
Shifu33 <appeliustheo@gmail.com>
2017-01-18 17:15:51 +0100

    ajax favoris piecesFUll

--- app/Views/front/Items/piecesFull.php
+++ app/Views/front/Items/piecesFull.php
@@ -87,9 +87,9 @@ Vous trouverez également des pièces détachées d'origine de la marque PLAYMOB
 								<span style="cursor:pointer;">
 									<?php if(!empty($_SESSION['user'])): ?>
 										<?php if(in_array($product['id'], $favorite)): ?>
-											<button class="favorite" type="submit" name="<?=str_replace(' ', '', $product['name']);?>" value="<?=$product['id']?>"><i class="fa fa-heart fa-fw favoriteicon_original favoriteicon_click" aria-hidden="true" style="color: #c11131;" title="Ajouter à mes favoris"></i></button> <!-- l'icône ne change pas de couleur, pour ça que j'ai mis 'ee' pour voir la différence lors du dev -->
+											<button class="favorite" type="submit" name="<?=str_replace(' ', '', $product['name']);?>" value="<?=$product['id']?>" data-id="<?=$product['id'];?>"><i class="fa fa-heart fa-fw favoriteicon_original favoriteicon_click" aria-hidden="true" style="color: #c11131;" title="Ajouter à mes favoris"></i></button> <!-- l'icône ne change pas de couleur, pour ça que j'ai mis 'ee' pour voir la différence lors du dev -->
 										<?php else: ?>
-											<button class="favorite" type="submit" name="<?=str_replace(' ', '', $product['name']);?>" value="<?=$product['id'];?>"><i class="fa fa-heart-o fa-fw favoriteicon_original favoriteicon_click" aria-hidden="true" title="Ajouter à mes favoris"></i></button>
+											<button class="favorite" type="submit" name="<?=str_replace(' ', '', $product['name']);?>" value="<?=$product['id'];?>" data-id="<?=$product['id'];?>"><i class="fa fa-heart-o fa-fw favoriteicon_original favoriteicon_click" aria-hidden="true" title="Ajouter à mes favoris"></i></button>
 										<?php endif; ?>
 									<?php else : ?>
 										<a href="<?=$this->url('login');?>"><i class="fa fa-heart-o fa-fw favoriteicon_original favoriteicon_click" aria-hidden="true" title="Ajouter à mes favoris"></i></a>
@@ -280,27 +280,27 @@ Vous trouverez également des pièces détachées d'origine de la marque PLAYMOB
 </div>
 <?php $this->stop('main_content') ?>
 
-<?php $this->start('js')?>
+<?php $this->start('js') ?>
 	<script>
 		$(document).ready(function(){
-			$('.add_to_shopping_cart').click(function(e){
+			$('.favorite').click(function(e){
 				e.preventDefault();
 
-				var idProduct = $(this).data('id');
+				var idFavorite = $(this).data('id');
 
 				$.ajax({
-  					url: '<?=$this->url('ajax_addToCart'); ?>',
+					url: '<?=$this->url('ajax_favorite');?>',
 					type: 'post',
 					cache: false,
-					data: {id_product: idProduct},  // $_POST['id_product']
+					data: {id_item: idFavorite},
 					dataType: 'json',
-					success: function(out){
-						if(out.code == 'ok'){
-			  				$().html();	
+					success: function(add){
+						if(add.msg == 'ok'){
+							$('body').load('<?=$this->url('listItemPiecesFull');?>');
 						}
 					}
-  				});
+				});
 			});
 		});
 	</script>
-<?php $this->stop('js')?>
+<?php $this->stop('js') ?>
