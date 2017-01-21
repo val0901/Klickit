2172211fc966209e429d416ed484f9ff4827634d
Dankest doggo eva <a7x.tholomew.plague@hotmail.fr>
2017-01-21 15:36:31 +0100

    préparation pour rajout dans la table order (AJAX)

--- app/Views/front/Order/orderList.php
+++ app/Views/front/Order/orderList.php
@@ -15,65 +15,75 @@
 	<br><br>
     
     <!--Title et border-->
-    <div class="order_box">
-        <h1 class="order_title">Récapitulatif de commande</h1>
-        <hr>
-    
-        <table class="table">
-            <thead> 
-                <tr> 
-                    <th>Article</th> 
-                    <th>Description</th> 
-                    <th>Prix unitaire</th> 
-                    <th>Quantité</th>
-                    <th>Total</th>
-                    <th></th>
-                </tr> 
-            </thead> 
-            <tbody>
-                <?php foreach($w_items as $item) :?> 
+    <form>
+        <div class="order_box">
+            <h1 class="order_title">Récapitulatif de commande</h1>
+            <hr>
+        
+            <table class="table">
+                <thead> 
                     <tr> 
-                        <td scope="row"><img class="img-responsive" src="<?=$this->assetUrl('art/'.$item['picture1']);?>" style="width: 15vw;margin:0 auto;"></td> 
-                        <td>
-                            <li class="ordertable_title"><?=$item['name']?><li>
-                            <li class="ordertable_text">Référence: <?=$item['id']?><li>
-                        </td>
+                        <th>Article</th> 
+                        <th>Description</th> 
+                        <th>Prix unitaire</th> 
+                        <th>Quantité</th>
+                        <th>Total</th>
+                        <th></th>
+                    </tr> 
+                </thead> 
+                <tbody>    
+                    <?php foreach($w_items as $item) :?> 
+                        <tr> 
+                            <td scope="row"><img class="img-responsive" src="<?=$this->assetUrl('art/'.$item['picture1']);?>" style="width: 15vw;margin:0 auto;"></td> 
+                            <td>
+                                <li class="ordertable_title"><?=$item['name']?><li>
+                                <li class="ordertable_text">Référence: <?=$item['id']?><li>
+                                <input type="hidden" name="id" value="<?=$item['id']?>">
+                            </td>
 
-                        <?php if($item['newPrice'] == 0) :?> 
-                            <td><p class="ordertable_title"><?=$item['price']?> €<p></td>
-                        <?php elseif($item['newPrice'] > 0) :?>
-                            <td><p class="ordertable_title"><?=$item['newPrice']?> €<p></td>
-                        <?php endif;?> 
+                            <?php if($item['newPrice'] == 0) :?> 
+                                <td><p class="ordertable_title"><?=$item['price']?> €<p></td>
+                                <input type="hidden" name="price" value="<?=$item['price']?>">
+                            <?php elseif($item['newPrice'] > 0) :?>
+                                <td><p class="ordertable_title"><?=$item['newPrice']?> €<p></td>
+                                <input type="hidden" name="price" value="<?=$item['newPrice']?>">
+                            <?php endif;?> 
 
-                        <td><p class="ordertable_title"><input type="number" name="number" id="number" value="<?=$item['qt']?>"><p></td>
+                            <td><p class="ordertable_title"><input type="number" name="quantity" id="number" value="<?=$item['qt']?>"><p></td>
 
-                        <?php if($item['newPrice'] == 0) :?>  
-                            <td><p class="ordertable_title"><?=$item['qt']*$item['price']?> €<p></td>
-                        <?php elseif($item['newPrice'] > 0) :?>
-                            <td><p class="ordertable_title"><?=$item['qt']*$item['newPrice']?> €<p></td>
-                        <?php endif; ?>       
-                        <td><p class="ordertable_title"><i class="fa fa-trash" aria-hidden="true" style="color: #000;" data-id="<?=$item['id']?>"></i><p></td>
-                    </tr>
-                <?php endforeach; ?>    
-            </tbody> 
-        </table>
-		<div class="orderlist_total">
-			<ul  style="float:right;">
-				<li>
-					<p><?=$total?> €</p>
-					<p><?=$fdp?> €</p>
-					<p><strong><?=$fdp + $total?> €</strong></p>
-				</li>
-			</ul>
-			<ul>
-				<li>
-					<p>Total articles :</p>
-					<p>Frais de port :</p>
-					<p>TOTAL COMMANDE :</p>
-				</li>
-			</ul>
-		</div>
-    </div>
+                            <?php if($item['newPrice'] == 0) :?>  
+                                <td><p class="ordertable_title"><?=$item['qt']*$item['price']?> €<p></td>
+                            <?php elseif($item['newPrice'] > 0) :?>
+                                <td><p class="ordertable_title"><?=$item['qt']*$item['newPrice']?> €<p></td>
+                            <?php endif; ?>       
+                            <td><p class="ordertable_title"><i class="fa fa-trash" aria-hidden="true" style="color: #000;" data-id="<?=$item['id']?>"></i><p></td>
+                        </tr>
+                    <?php endforeach; ?>                  
+                </tbody> 
+            </table>
+    		<div class="orderlist_total">
+    			<ul  style="float:right;">
+    				<li>
+    					<p><?=$total?> €</p>
+                        <input type="hidden" name="sub_total" value="<?=$total?>">
+    					<p><?=$fdp?> €</p>
+                        <input type="hidden" name="shipping" value="<?=$fdp?>">
+    					<p><strong><?=$fdp + $total?> €</strong></p>
+                        <input type="hidden" name="total" value="<?=$fdp+$total?>">
+
+
+    				</li>
+    			</ul>
+    			<ul>
+    				<li>
+    					<p>Total articles :</p>
+    					<p>Frais de port :</p>
+    					<p>TOTAL COMMANDE :</p>
+    				</li>
+    			</ul>
+    		</div>
+        </div>
+    </form>  
     <!--End title et border-->
     
     <!--Frais de port-->
@@ -98,7 +108,7 @@
             <button type="button" class="btn btn-primary order_buttonpadding ordercontinu_color">CONTINUER MES ACHATS</button>
         </div>
         <div class="col-md-6 order_buttonscapR">
-            <button type="button" class="btn btn-primary order_buttonpadding ordercommander_color">COMMANDER</button>
+            <button type="button" class="btn btn-primary order_buttonpadding ordercommander_color get-order">COMMANDER</button>
         </div>
     </div>
     <!--End order buttons-->
@@ -123,6 +133,27 @@
                     success: function(out){
                         if(out.code == 'ok'){
                             window.location.href= window.location.href;
+                        }
+                    }
+                });
+            });
+        });
+    </script>
+
+    <script>
+        $(document).ready(function(){
+            $('.get-order').click(function(e){
+                e.preventDefault();
+
+                $.ajax({
+                    url: '<?=$this->url('ajax_deleteArt'); ?>',
+                    type: 'post',
+                    cache: false,
+                    data: $('form').serialize(),  // $_POST['id_product']
+                    dataType: 'json',
+                    success: function(out){
+                        if(out.code == 'ok'){
+                            console.log('yes');
                         }
                     }
                 });
