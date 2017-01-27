91e466a8c829479cbe0af8ef69f3e44afa3a2a78
Shifu33 <appeliustheo@gmail.com>
2017-01-27 15:00:00 +0100

    filtre

--- app/Views/back/Item/addItem.php
+++ app/Views/back/Item/addItem.php
@@ -121,7 +121,7 @@
 
 		<!-- Multiple Checkboxes -->
 		<div class="form-group">
-		  <label class="col-md-4 control-label" for="checkboxes">Multiple Checkboxes</label>
+		  <label class="col-md-4 control-label" for="checkboxes">Filtre :</label>
 		  <div class="col-md-4">
 		  <?php foreach($filter as $value): ?>
 			  <div class="checkbox">
