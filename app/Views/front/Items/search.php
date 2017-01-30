c56f79fa73d97c23acb1dead59e283ef537fffb5
Shifu33 <appeliustheo@gmail.com>
2017-01-30 16:44:23 +0100

    condition si aucun article trouvé

--- app/Views/front/Items/search.php
+++ app/Views/front/Items/search.php
@@ -2,53 +2,60 @@
 
 <?php $this->start('main_content') ?>
 <div class="container_general">
-	<div class="row">
-		<div>
-			<?php if($countItem > 1): ?>
-				<p><?=$countItem;?> résultats trouvés</p>
-			<?php elseif($countItem === 1): ?>
-				<p><?=$countItem;?> résultat trouvé</p>
-			<?php endif; ?>
+	<?php if($countItem === 0): ?>
+		<div class="row">
+			<h2 class="no_result">Aucun résultat trouvé !</h2>
 		</div>
-		<?php foreach ($items as $value) : ?>
-			<?php $product = $findItem->findItems($value); ?>
-			<div class="col-md-3 col-xs-6 viewcategoryrow2col1_img">
-             <a href="<?=$this->url('viewArt', ['id' => $product['id']]);?>"><img src="<?=$this->assetUrl('art/'.$product['picture1']);?>" alt="photo de playmobil" class="img-thumbnail" style=""></a>
-				<div class="viewcategorycaption">
-					<?php if($product['newPrice'] == 0) : ?>
-						<h4><?=$product['price'];?>€</h4>
-					<?php else : ?>
-						<h4><span class="viewcategoryprixpromo"><?=$product['newPrice'];?>€</span> <span class="viewcategoryprixdelete"><?=$product['price'];?>€</span></h4>
-					<?php endif; ?>
-
-					<p class="iconeFavorite">
-						<span style="cursor:pointer;">
-							<?php if(!empty($_SESSION['user'])): ?>
-								<?php if(in_array($product['id'], $favorite)): ?>
-									<button class="favorite" type="submit" name="<?=str_replace(' ', '', $product['name']);?>"product="<?=$product['id']?>" data-id="<?=$product['id'];?>"><i class="fa fa-heart fa-fw favoriteicon_original favoriteicon_click" aria-hidden="true" style="color: #c11131;" title="Ajouter à mes favoris"></i></button>
-								<?php else: ?>
-									<button class="favorite" type="submit" name="<?=str_replace(' ', '', $product['name']);?>"product="<?=$product['id'];?>" data-id="<?=$product['id'];?>"><i class="fa fa-heart-o fa-fw favoriteicon_original favoriteicon_click" aria-hidden="true" title="Ajouter à mes favoris"></i></button>
-								<?php endif; ?>
-							<?php else : ?>
-								<a href="<?=$this->url('login');?>"><i class="fa fa-heart-o fa-fw favoriteicon_original favoriteicon_click" aria-hidden="true" title="Ajouter à mes favoris"></i></a>
-							<?php endif; ?>
-						</span> <?=$product['name'];?>
-					</p>
-
-					<?php if($product['statut'] == 'nouveaute'):?>
-						<div class="viewcategory_nouveau"><?=$product['statut'];?></div>
-					<?php elseif($product['statut'] == 'promotion'):?>
-						<div class="viewcategory_promo"><?=$product['statut'];?></div>
-					<?php elseif($product['statut'] == 'defaut'): ?>
-						<div class="viewcategory_defaut"></div>
-					<?php endif; ?>
-                </div>
-				<div class="viewcategory_button">
-					<button type="submit" class="btn btn-primary viewcategory_button_size add_to_shopping_cart" data-id="<?=$product['id']?>">ajouter au panier</button>
-				</div>
+	<?php elseif($countItem > 0): ?>
+		<div class="row">
+			<div>
+				<?php if($countItem > 1): ?>
+					<p class="result"><?=$countItem;?> résultats trouvés</p>
+				<?php elseif($countItem === 1): ?>
+					<p class="result"><?=$countItem;?> résultat trouvé</p>
+				<?php endif; ?>
 			</div>
-		<?php endforeach; ?>
-	</div>
+			<?php foreach ($items as $value) : ?>
+				<?php $product = $findItem->findItems($value); ?>
+				<div class="col-md-3 col-xs-6 viewcategoryrow2col1_img">
+	             <a href="<?=$this->url('viewArt', ['id' => $product['id']]);?>"><img src="<?=$this->assetUrl('art/'.$product['picture1']);?>" alt="photo de playmobil" class="img-thumbnail" style=""></a>
+					<div class="viewcategorycaption">
+						<?php if($product['newPrice'] == 0) : ?>
+							<h4><?=$product['price'];?>€</h4>
+						<?php else : ?>
+							<h4><span class="viewcategoryprixpromo"><?=$product['newPrice'];?>€</span> <span class="viewcategoryprixdelete"><?=$product['price'];?>€</span></h4>
+						<?php endif; ?>
+
+						<p class="iconeFavorite">
+							<span style="cursor:pointer;">
+								<?php if(!empty($_SESSION['user'])): ?>
+									<?php if(in_array($product['id'], $favorite)): ?>
+										<button class="favorite" type="submit" name="<?=str_replace(' ', '', $product['name']);?>"product="<?=$product['id']?>" data-id="<?=$product['id'];?>"><i class="fa fa-heart fa-fw favoriteicon_original favoriteicon_click" aria-hidden="true" style="color: #c11131;" title="Ajouter à mes favoris"></i></button>
+									<?php else: ?>
+										<button class="favorite" type="submit" name="<?=str_replace(' ', '', $product['name']);?>"product="<?=$product['id'];?>" data-id="<?=$product['id'];?>"><i class="fa fa-heart-o fa-fw favoriteicon_original favoriteicon_click" aria-hidden="true" title="Ajouter à mes favoris"></i></button>
+									<?php endif; ?>
+								<?php else : ?>
+									<a href="<?=$this->url('login');?>"><i class="fa fa-heart-o fa-fw favoriteicon_original favoriteicon_click" aria-hidden="true" title="Ajouter à mes favoris"></i></a>
+								<?php endif; ?>
+							</span> <?=$product['name'];?>
+						</p>
+
+						<?php if($product['statut'] == 'nouveaute'):?>
+							<div class="viewcategory_nouveau"><?=$product['statut'];?></div>
+						<?php elseif($product['statut'] == 'promotion'):?>
+							<div class="viewcategory_promo"><?=$product['statut'];?></div>
+						<?php elseif($product['statut'] == 'defaut'): ?>
+							<div class="viewcategory_defaut"></div>
+						<?php endif; ?>
+	                </div>
+					<div class="viewcategory_button">
+						<button type="submit" class="btn btn-primary viewcategory_button_size add_to_shopping_cart" data-id="<?=$product['id']?>">ajouter au panier</button>
+					</div>
+				</div>
+			<?php endforeach; ?>
+		</div>
+	<?php endif; ?>
+</div>
 <?php $this->stop('main_content') ?>
 
 <script>
