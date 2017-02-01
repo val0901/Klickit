5de41324ebb1e76eb473422bead995fbc290e6b2
val0901 <symezac.valerie@neuf.fr>
2017-02-01 18:10:47 +0100

    page team en cours

--- app/Views/front/Pages/team.php
+++ app/Views/front/Pages/team.php
@@ -2,7 +2,82 @@
 
 <?php $this->start('main_content') ?>
 
-Présentation de l'équipe de création
+	<h1>Présentation de l'équipe de création</h1>
+
+	<div class="containerTeam">
+
+	<div class="childTeam">
+		<div class="wrap">
+		  <div class="text-part">
+		    Valérie Baldy-Symezac
+		  </div>
+		  <div class="image-part">
+		    <img src="<?=$this->assetUrl('/img/valerie.png');?>">
+		  </div>
+		</div>
+		<div class=text-description>
+		  	Développeuse Back-End
+		</div>
+	</div>
+
+	<div class="childTeam">
+		<div class="wrap">
+		  <div class="text-part">
+		    Valérie Baldy-Symezac
+		  </div>
+		  <div class="image-part">
+		    <img src="<?=$this->assetUrl('/img/valerie.png');?>">
+		  </div>
+		</div>
+		<div class=text-description>
+		  	Développeuse Back-End
+		</div>
+	</div>
+
+	<div class="childTeam">
+		<div class="wrap">
+		  <div class="text-part">
+		    Valérie Baldy-Symezac
+		  </div>
+		  <div class="image-part">
+		    <img src="<?=$this->assetUrl('/img/valerie.png');?>">
+		  </div>
+		</div>
+		<div class=text-description>
+		  	Développeuse Back-End
+		</div>
+	</div>
+
+	<div class="childTeam">
+		<div class="wrap">
+		  <div class="text-part">
+		    Valérie Baldy-Symezac
+		  </div>
+		  <div class="image-part">
+		    <img src="<?=$this->assetUrl('/img/valerie.png');?>">
+		  </div>
+		</div>
+		<div class=text-description>
+		  	Développeuse Back-End
+		</div>
+	</div>
+
+	<div class="childTeam">
+		<div class="wrap">
+		  <div class="text-part">
+		    Valérie Baldy-Symezac
+		  </div>
+		  <div class="image-part">
+		    <img src="<?=$this->assetUrl('/img/valerie.png');?>">
+		  </div>
+		</div>
+		<div class=text-description>
+		  	Développeuse Back-End
+		</div>
+	</div>
+
+	</div> <!-- fin container -->
+
 
 
 <?php $this->stop('main_content') ?>
