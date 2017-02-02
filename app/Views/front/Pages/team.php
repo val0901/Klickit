543e042f6fb94afcc63b975a7027928365bdeb02
cec33 <contact@emaginetapub.com>
2017-02-02 17:06:31 +0100

    page team

--- app/Views/front/Pages/team.php
+++ app/Views/front/Pages/team.php
@@ -2,83 +2,116 @@
 
 <?php $this->start('main_content') ?>
 
-	<div id="titleTeam">
-		<h1>Présentation de l'équipe de création</h1>
-	</div>
+<div id="titleTeam">
+    <h1 class="titleteam">Présentation de l'équipe de création</h1>
+</div>
+<hr class="lineteam">
+<div class="containerTeam">
 
-	<div class="containerTeam">
+    <div class="childTeam">
+        <div class="wrap">
+            <div class="text-part text-color1">
+                Théo Appelius
+            </div>
+            <div class="image-part">
+                <img src="<?=$this->assetUrl('/img/theo-appelius.jpg');?>">
+            </div>
+        </div>
+        <h2 class="h2team">            
+            Développeur Back End    
+        </h2>
+        <div class=text-description>
+            Proinde die funestis interrogationibus praestituto imaginarius iudex equitum resedit magister adhibitis aliis iam quae essent agenda praedoctis, et adsistebant hinc inde notarii, quid quaesitum esset, quidve responsum, cursim ad Caesarem perferentes, cuius imperio truci, stimulis reginae exsertantis aurem subinde per aulaeum, nec diluere obiecta permissi nec defensi periere conplures.
+<br><br>
+            Dum apud Persas, ut supra narravimus, perfidia regis motus agitat insperatos, et in eois tractibus bella rediviva consurgunt, anno sexto decimo et eo diutius post Nepotiani exitium, saeviens per urbem aeternam urebat cuncta Bellona, ex primordiis minimis ad clades excita luctuosas, quas obliterasset utinam iuge silentium! ne forte paria quandoque temptentur, plus exemplis generalibus nocitura quam delictis.
 
-	<div class="childTeam">
-		<div class="wrap">
-		  <div class="text-part text-color1">
-		    Théo Appelius
-		  </div>
-		  <div class="image-part">
-		    <img src="<?=$this->assetUrl('/img/valerie.png');?>">
-		  </div>
-		</div>
-		<div class=text-description>
-		  	Développeur Back-End  
-		</div>
-	</div>
+        </div>
+    </div>
 
-	<div class="childTeam">
-		<div class="wrap">
-		  <div class="text-part text-color2">
-		    Valérie Baldy-Symezac
-		  </div>
-		  <div class="image-part">
-		    <img src="<?=$this->assetUrl('/img/valerie.png');?>">
-		  </div>
-		</div>
-		<div class=text-description>
-		  	Développeuse Back-End
-		</div>
-	</div>
+    <div class="childTeam">
+        <div class="wrap">
+            <div class="text-part text-color2">
+                Valérie Baldy-Symezac
+            </div>
+            <div class="image-part">
+                <img src="<?=$this->assetUrl('/img/valerie-baldy-symezac2.jpg');?>">
+            </div>
+        </div>
+        <h2 class="h2team">            
+            Développeur Back End    
+        </h2>
+        <div class=text-description>
+            Proinde die funestis interrogationibus praestituto imaginarius iudex equitum resedit magister adhibitis aliis iam quae essent agenda praedoctis, et adsistebant hinc inde notarii, quid quaesitum esset, quidve responsum, cursim ad Caesarem perferentes, cuius imperio truci, stimulis reginae exsertantis aurem subinde per aulaeum, nec diluere obiecta permissi nec defensi periere conplures.
+<br><br>
+            Dum apud Persas, ut supra narravimus, perfidia regis motus agitat insperatos, et in eois tractibus bella rediviva consurgunt, anno sexto decimo et eo diutius post Nepotiani exitium, saeviens per urbem aeternam urebat cuncta Bellona, ex primordiis minimis ad clades excita luctuosas, quas obliterasset utinam iuge silentium! ne forte paria quandoque temptentur, plus exemplis generalibus nocitura quam delictis.
 
-	<div class="childTeam">
-		<div class="wrap">
-		  <div class="text-part text-color3">
-		    Cécile Lafont
-		  </div>
-		  <div class="image-part">
-		    <img src="<?=$this->assetUrl('/img/valerie.png');?>">
-		  </div>
-		</div>
-		<div class=text-description>
-		  	Développeuse Front-End
-		</div>
-	</div>
+        </div>
+    </div>
 
-	<div class="childTeam">
-		<div class="wrap">
-		  <div class="text-part text-color4">
-		    Landy Razafindrabe
-		  </div>
-		  <div class="image-part">
-		    <img src="<?=$this->assetUrl('/img/valerie.png');?>">
-		  </div>
-		</div>
-		<div class=text-description>
-		  	Développeur Back-End
-		</div>
-	</div>
+    <div class="childTeam">
+        <div class="wrap">
+            <div class="text-part text-color3">
+                Cécile Lafont
+            </div>
+            <div class="image-part">
+                <img src="<?=$this->assetUrl('/img/cecile-lafont3.jpg');?>">
+            </div>
+        </div>
+        <h2 class="h2team">
+            Développeur Front End
+        </h2>
+        <div class=text-description>
+            Proinde die funestis interrogationibus praestituto imaginarius iudex equitum resedit magister adhibitis aliis iam quae essent agenda praedoctis, et adsistebant hinc inde notarii, quid quaesitum esset, quidve responsum, cursim ad Caesarem perferentes, cuius imperio truci, stimulis reginae exsertantis aurem subinde per aulaeum, nec diluere obiecta permissi nec defensi periere conplures.
+<br><br>
+            Dum apud Persas, ut supra narravimus, perfidia regis motus agitat insperatos, et in eois tractibus bella rediviva consurgunt, anno sexto decimo et eo diutius post Nepotiani exitium, saeviens per urbem aeternam urebat cuncta Bellona, ex primordiis minimis ad clades excita luctuosas, quas obliterasset utinam iuge silentium! ne forte paria quandoque temptentur, plus exemplis generalibus nocitura quam delictis.
 
-	<div class="childTeam">
-		<div class="wrap">
-		  <div class="text-part text-color5">
-		    Jie Xu
-		  </div>
-		  <div class="image-part">
-		    <img src="<?=$this->assetUrl('/img/valerie.png');?>">
-		  </div>
-		</div>
-		<div class=text-description>
-		  	Développeuse Front-End
-		</div>
-	</div>
 
-	</div> <!-- fin container -->
+        </div>
+    </div>
+
+    <div class="childTeam">
+        <div class="wrap">
+            <div class="text-part text-color4">
+                Landy Razafindrabe
+            </div>
+            <div class="image-part">
+                <img src="<?=$this->assetUrl('/img/landy.jpg');?>">
+            </div>
+        </div>
+        <h2 class="h2team">            
+            Développeur Back End    
+        </h2>
+        <div class=text-description>
+            Proinde die funestis interrogationibus praestituto imaginarius iudex equitum resedit magister adhibitis aliis iam quae essent agenda praedoctis, et adsistebant hinc inde notarii, quid quaesitum esset, quidve responsum, cursim ad Caesarem perferentes, cuius imperio truci, stimulis reginae exsertantis aurem subinde per aulaeum, nec diluere obiecta permissi nec defensi periere conplures.
+<br><br>
+            Dum apud Persas, ut supra narravimus, perfidia regis motus agitat insperatos, et in eois tractibus bella rediviva consurgunt, anno sexto decimo et eo diutius post Nepotiani exitium, saeviens per urbem aeternam urebat cuncta Bellona, ex primordiis minimis ad clades excita luctuosas, quas obliterasset utinam iuge silentium! ne forte paria quandoque temptentur, plus exemplis generalibus nocitura quam delictis.
+
+
+        </div>
+    </div>
+
+    <div class="childTeam">
+        <div class="wrap">
+            <div class="text-part text-color5">
+                Jie Xu
+            </div>
+            <div class="image-part">
+                <img src="<?=$this->assetUrl('/img/jie-xu2.jpg');?>">
+            </div>
+        </div>
+        <h2 class="h2team">            
+            Développeur Front End    
+        </h2>
+
+        <div class=text-description>
+            Proinde die funestis interrogationibus praestituto imaginarius iudex equitum resedit magister adhibitis aliis iam quae essent agenda praedoctis, et adsistebant hinc inde notarii, quid quaesitum esset, quidve responsum, cursim ad Caesarem perferentes, cuius imperio truci, stimulis reginae exsertantis aurem subinde per aulaeum, nec diluere obiecta permissi nec defensi periere conplures.
+<br><br>
+            Dum apud Persas, ut supra narravimus, perfidia regis motus agitat insperatos, et in eois tractibus bella rediviva consurgunt, anno sexto decimo et eo diutius post Nepotiani exitium, saeviens per urbem aeternam urebat cuncta Bellona, ex primordiis minimis ad clades excita luctuosas, quas obliterasset utinam iuge silentium! ne forte paria quandoque temptentur, plus exemplis generalibus nocitura quam delictis.
+
+        </div>
+    </div>
+
+</div> <!-- fin container -->
 
 
 
