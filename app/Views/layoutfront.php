2f0b60aa6f7183b7da13179f50153b83c88a2e97
cec33 <contact@emaginetapub.com>
2017-02-06 16:13:07 +0100

    affichage panier

--- app/Views/layoutfront.php
+++ app/Views/layoutfront.php
@@ -121,22 +121,22 @@
 							<div id="view" class="row item_cart" style="margin: 10px 10px 0 10px;">
 								<?php if(!empty($w_items)): ?>
 									<?php foreach($w_items as $item) : ?>
-										<div class="col-xs-6">
+										<div class="col-xs-10">
 											<?=$item['name']?>
 										</div>
 
 										<?php if($item['newPrice'] == 0) : ?>
-											<div class="col-xs-6">
+											<div class="col-xs-2">
 												<?= $item['price'] ?>€
 											</div>
 										<?php else : ?>
-											<div class="col-xs-6">
+											<div class="col-xs-2">
 												<?= $item['newPrice'] ?>€
 											</div>
 										<?php endif; ?>
 									<?php endforeach; ?>
 								<?php else : ?>
-									<div class="col-xs-6" style="color:red;">
+									<div class="col-xs-12" style="color:red;">
 										Vous n'avez pas d'article dans votre panier.
 									</div>	
 								<?php endif; ?>			
