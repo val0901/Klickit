30e84de4c3ca08f87361ce03577399af09b48f84
cec33 <contact@emaginetapub.com>
2017-02-06 16:44:18 +0100

    modif présentation mentions légales

--- app/Views/front/Pages/legalMention.php
+++ app/Views/front/Pages/legalMention.php
@@ -10,17 +10,17 @@ Responsable editorial : Laurent Lafont
 Téléphone :06 11 82 17 71 - Fax : -
 Email : contact@klickit.fr
 Site Web : <a href="http://www.klickit.fr" style="color:#ff931e;">www.klickit.fr</a>
-</br>
+<br><br>
 <p style="color: #b51a00;"><b><span style="color: rgb(0, 0, 0);">Hébergement :</span> </b></p>
 Hébergeur : OVH
 2 rue Kellermann, BP 80157, 59100 Roubaix
 Site Web :  <a href="http://http://www.ovh.com" style="color:#ff931e;">http://www.ovh.com</a>
-</br>
+<br><br>
 <p style="color: #b51a00;"><span style="color: rgb(0, 0, 0);"><b>Développement</b><b> : </b></span></p>
 Front-End : Jie Xu, Cécile Lafont - Back-End : Landy Razafindrabe, Valérie Baldy-Symezac, Théo Appelius
 Adresse : Equipe issue de WebForce3
 Site Web : <a href="http://http://www.klickit.fr/public/team" style="color:#ff931e;">voir l'équipe</a>
-</br>
+<br><br>
 <p style="color: #b51a00;"><span style="color: rgb(0, 0, 0);"><b>Conditions d’utilisation : </b></span></p>
 Ce site (<a href="http://www.klickit.fr" style="color:#ff931e;">www.klickit.fr</a>) est proposé en différents langages web (HTML, HTML5, Javascript, CSS, etc…) pour un meilleur confort d'utilisation et un graphisme plus agréable, nous vous recommandons de recourir à des navigateurs modernes comme Internet explorer, Safari, Firefox, Google Chrome, etc…
 Les mentions légales ont été générées sur le site <a title="générateur de mentions légales pour site internet gratuit" href="http://www.generateur-de-mentions-legales.com" style="color:#ff931e;">Générateur de mentions légales</a>, offert par <a title="imprimerie paris, imprimeur paris" href="http://welye.com" style="color:#ff931e;">Welye</a>.
@@ -34,29 +34,29 @@ Les mentions légales ont été générées sur le site <a title="générateur d
 Les utilisateurs, les abonnés et les visiteurs des sites internet de ne peuvent mettre en place un hyperlien en direction de ce site sans l'autorisation expresse et préalable de ©Klickit.
 
 Dans l'hypothèse où un utilisateur ou visiteur souhaiterait mettre en place un hyperlien en direction d’un des sites internet de ©Klickit, il lui appartiendra d'adresser un email accessible sur le site afin de formuler sa demande de mise en place d'un hyperlien. ©Klickit se réserve le droit d’accepter ou de refuser un hyperlien sans avoir à en justifier sa décision.
-</br>
+<br><br>
 <p style="color: #b51a00;"><span style="color: rgb(0, 0, 0);"><b>Services fournis : </b></span></p>
 <p style="color: #323333;">L'ensemble des activités de la société ainsi que ses informations sont présentés sur notre site <a href="http://www.klickit.fr" style="color:#ff931e;">www.klickit.fr</a>.</p>
 <p style="color: #323333;">©Klickit s’efforce de fournir sur le site www.klickit.fr des informations aussi précises que possible. les renseignements figurant sur le site <a href="http://www.klickit.fr" style="color:#ff931e;">www.klickit.fr</a> ne sont pas exhaustifs et les photos non contractuelles. Ils sont donnés sous réserve de modifications ayant été apportées depuis leur mise en ligne. Par ailleurs, tous les informations indiquées sur le site www.klickit.fr<span style="color: #000000;"><b> </b></span>sont données à titre indicatif, et sont susceptibles de changer ou d’évoluer sans préavis.  </p>
-</br>
+<br>
 <p style="color: #b51a00;"><span style="color: rgb(0, 0, 0);"><b>Limitation contractuelles sur les données : </b></span></p>
 Les informations contenues sur ce site sont aussi précises que possible et le site remis à jour à différentes périodes de l’année, mais peut toutefois contenir des inexactitudes ou des omissions. Si vous constatez une lacune, erreur ou ce qui parait être un dysfonctionnement, merci de bien vouloir le signaler par email, à l’adresse contact@klickit.fr, en décrivant le problème de la manière la plus précise possible (page posant problème, type d’ordinateur et de navigateur utilisé, …).
 
 Tout contenu téléchargé se fait aux risques et périls de l'utilisateur et sous sa seule responsabilité. En conséquence, ne saurait être tenu responsable d'un quelconque dommage subi par l'ordinateur de l'utilisateur ou d'une quelconque perte de données consécutives au téléchargement. <span style="color: #323333;">De plus, l’utilisateur du site s’engage à accéder au site en utilisant un matériel récent, ne contenant pas de virus et avec un navigateur de dernière génération mis-à-jour</span>
 
 Les liens hypertextes mis en place dans le cadre du présent site internet en direction d'autres ressources présentes sur le réseau Internet ne sauraient engager la responsabilité de ©Klickit.
-</br>
+<br><br>
 <p style="color: #b51a00;"><span style="color: rgb(0, 0, 0);"><b>Propriété intellectuelle :</b></span></p>
 Tout le contenu du présent sur le site <a href="http://www.klickit.fr" style="color:#ff931e;">www.klickit.fr</a>, incluant, de façon non limitative, les graphismes, images, textes, vidéos, animations, sons, logos, gifs et icônes ainsi que leur mise en forme sont la propriété exclusive de la société à l'exception des marques, logos ou contenus appartenant à d'autres sociétés partenaires ou auteurs.
 
 Toute reproduction, distribution, modification, adaptation, retransmission ou publication, même partielle, de ces différents éléments est strictement interdite sans l'accord exprès par écrit de ©Klickit. Cette représentation ou reproduction, par quelque procédé que ce soit, constitue une contrefaçon sanctionnée par les articles L.335-2 et suivants du Code de la propriété intellectuelle. Le non-respect de cette interdiction constitue une contrefaçon pouvant engager la responsabilité civile et pénale du contrefacteur. En outre, les propriétaires des Contenus copiés pourraient intenter une action en justice à votre encontre.
-</br>
+<br><br>
 <p style="color: #b51a00;"><span style="color: rgb(0, 0, 0);"><b>Déclaration à la CNIL : </b></span></p>
 Conformément à la loi 78-17 du 6 janvier 1978 (modifiée par la loi 2004-801 du 6 août 2004 relative à la protection des personnes physiques à l'égard des traitements de données à caractère personnel) relative à l'informatique, aux fichiers et aux libertés, ce site a fait l'objet d'une déclaration  auprès de la Commission nationale de l'informatique et des libertés (<a href="http://www.cnil.fr/" style="color:#ff931e;">www.cnil.fr</a>).
-</br>
+<br><br>
 <p style="color: #b51a00;"><span style="color: rgb(0, 0, 0);"><b>Litiges : </b></span></p>
 Les présentes conditions du site <a href="http://www.klickit.fr" style="color:#ff931e;">www.klickit.fr</a> sont régies par les lois françaises et toute contestation ou litiges qui pourraient naître de l'interprétation ou de l'exécution de celles-ci seront de la compétence exclusive des tribunaux dont dépend le siège social de la société. La langue de référence, pour le règlement de contentieux éventuels, est le français.
-</br>
+<br><br>
 <p style="color: #b51a00;"><span style="color: rgb(0, 0, 0);"><b>Données personnelles :</b></span></p>
 De manière générale, vous n’êtes pas tenu de nous communiquer vos données personnelles lorsque vous visitez notre site Internet <a href="http://www.klickit.fr" style="color:#ff931e;">www.klickit.fr</a>.
 
