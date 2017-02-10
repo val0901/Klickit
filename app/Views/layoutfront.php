eb32095db2b389d9a236e54f768b42adaef1c2f7
cec33 <contact@emaginetapub.com>
2017-02-10 18:58:04 +0100

    modif panier

--- app/Views/layoutfront.php
+++ app/Views/layoutfront.php
@@ -124,18 +124,24 @@
 								<div id="view" class="row item_cart" style="margin: 10px 10px 0 10px;">
 									<?php if(!empty($w_items)): ?>
 										<?php foreach($w_items as $item) : ?>
-											<div class="col-xs-10">
+											<div class="col-xs-8 basketqt">
 												<?=$item['name']?>
-
 											</div>
 
 											<?php if($item['newPrice'] == 0) : ?>
-												<div class="col-xs-2">
-													<?= $item['price'] ?>€ x <?=$item['qt'];?>
+												<div class="col-xs-2 text-left">
+													x <?=$item['qt'];?>
 												</div>
+												<div class="col-xs-2 text-right ">
+													<?= $item['price'] ?>€
+												</div>
+                                    
 											<?php else : ?>
-												<div class="col-xs-2">
-													<?= $item['newPrice'] ?>€ x <?=$item['qt'];?>
+												<div class="col-xs-2 text-left">
+													x <?=$item['qt'];?>
+												</div>
+												<div class="col-xs-2 text-right ">
+													<?= $item['newPrice'] ?>€
 												</div>
 											<?php endif; ?>
 										<?php endforeach; ?>
@@ -156,12 +162,12 @@
 									</div>
 								</div>
 	-->
-								<div class="row" style="margin: 0 10px;border-top:1px dotted #000;padding-top:10px;">
-									<div class="col-xs-6 shoppingmenu_total">
-										<p>Total: <?=$w_total;?></p>
+								<div class="row sumbacket text-right" >
+									<div class="text-left text-uppercase col-xs-6 shoppingmenu_total">
+										Total :
 									</div>
-									<div class="col-xs-6 shoppingmenu_total price" style="text-align:right;">
-										<!--ajout du prix en AJAX-->
+									<div class="col-xs-6 shoppingmenu_total price text-right">
+										<?=$w_total;?>€
 									</div>
 								</div>
 							</div>
