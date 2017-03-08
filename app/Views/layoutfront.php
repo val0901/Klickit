6dbbc9da1c557287445676fedbb47b06614b1a18
cec33 <contact@emaginetapub.com>
2017-03-08 10:38:15 +0100

    correct deconnect button

--- app/Views/layoutfront.php
+++ app/Views/layoutfront.php
@@ -183,7 +183,11 @@
 					<!--icon-cog menu-->
                     <li class="span_float">
                     	<?php if(!empty($_SESSION['user']) && isset($_SESSION['user'])): ?>
-                        	<form><button id="logout" type="submit" data-id="login_out"><i class="fa fa-sign-out fa-5x icon_cursor navR_color" aria-hidden="true" id="cogicon_click" title="Deconnexion" style="color: #ea2229;"></i></button></form>
+                        	<form>
+                                <button id="logout" type="submit" data-id="login_out">
+                                    <i id="logout" class="fa fa-sign-out fa-5x icon_cursor" aria-hidden="true" id="cogicon_click" title="Deconnexion" style="color: #ea2229;"></i>
+                                </button>
+                            </form>
                         <?php endif; ?>
                     </li>
                     <!--End icon-cog menu-->
