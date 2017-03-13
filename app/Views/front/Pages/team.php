3b437f96083ed390830620b5362d70fbbe6c1ad4
cec33 <contact@emaginetapub.com>
2017-03-13 13:30:39 +0100

    page équipe

--- app/Views/front/Pages/team.php
+++ app/Views/front/Pages/team.php
@@ -11,21 +11,26 @@
     <div class="childTeam">
         <div class="wrap">
             <div class="text-part text-color1">
-                Théo Appelius
+                Développeur BACK
             </div>
             <div class="image-part">
                 <img src="<?=$this->assetUrl('/img/theo-appelius.jpg');?>">
             </div>
         </div>
         <h2 class="h2team">            
-            Développeur Back End    
+            Développeur web junior    
         </h2>
         <div class=text-description>
-            Proinde die funestis interrogationibus praestituto imaginarius iudex equitum resedit magister adhibitis aliis iam quae essent agenda praedoctis, et adsistebant hinc inde notarii, quid quaesitum esset, quidve responsum, cursim ad Caesarem perferentes, cuius imperio truci, stimulis reginae exsertantis aurem subinde per aulaeum, nec diluere obiecta permissi nec defensi periere conplures.
+            Sortant d'un Bac Pro Commerce, j'ai décidé de me réorienter vers un domaine qui me plaisait, j'ai donc trouvé mon bonheur dans le développement Web. 
+
             <br><br>
-            Dum apud Persas, ut supra narravimus, perfidia regis motus agitat insperatos, et in eois tractibus bella rediviva consurgunt, anno sexto decimo et eo diutius post Nepotiani exitium, saeviens per urbem aeternam urebat cuncta Bellona, ex primordiis minimis ad clades excita luctuosas, quas obliterasset utinam iuge silentium! ne forte paria quandoque temptentur, plus exemplis generalibus nocitura quam delictis.
+            Le challenge, me creuser la tête pour trouver des solutions techniques, me donner à fond pour réussir et réaliser mes objectifs, c'est ce qui me plaît en tant que développeur Back Junior. 
+
             <br><br>
-            <a class="linkteam" href="#" target="_blank">Contacter Théo</a>
+            À court terme je souhaite être indépendant en micro entreprise pour affiner mes compétences en Web. Mon objectif est d'apprendre des langages de programmation plus adaptés aux jeux vidéo pour être développeur jeux vidéo.              <br><br>
+            <a class="linkteam" href="https://www.linkedin.com/in/th%C3%A9o-appelius-19441b137/" target="_blank">Contacter Théo</a>
+            <br>
+            Twitter : @AppeliusTheo
         </div>
     </div>
 
@@ -65,13 +70,18 @@
             Développeur Front End
         </h2>
         <div class=text-description>
-            Proinde die funestis interrogationibus praestituto imaginarius iudex equitum resedit magister adhibitis aliis iam quae essent agenda praedoctis, et adsistebant hinc inde notarii, quid quaesitum esset, quidve responsum, cursim ad Caesarem perferentes, cuius imperio truci, stimulis reginae exsertantis aurem subinde per aulaeum, nec diluere obiecta permissi nec defensi periere conplures.
+            Expérience de 8 ans en Webdesign et Marketing Print et Web, suite à reconversion après 10 ans en gestion et comptabilité d'entreprise. Je maîtrise la suite Adobe et les outils du Web.
+
             <br><br>
-            Dum apud Persas, ut supra narravimus, perfidia regis motus agitat insperatos, et in eois tractibus bella rediviva consurgunt, anno sexto decimo et eo diutius post Nepotiani exitium, saeviens per urbem aeternam urebat cuncta Bellona, ex primordiis minimis ad clades excita luctuosas, quas obliterasset utinam iuge silentium! ne forte paria quandoque temptentur, plus exemplis generalibus nocitura quam delictis.
+            Je souhaite me perfectionner en développement Web et évoluer sur du développement mobile. L'entreprise idéale : agence Web ou SSII pour la diversité des projets. Poste idéal : évolution rapide sur un poste de responsable ou chef de projet Web et mobile.
+
+            <br><br>
+            Obtimiste, toujours à la recherche de solution pour évoluer, j'aime le travail d'équipe et m'adapte parfaitement au changement. 
             <br><br>
 
-            <a class="linkteam" href="http://cecilelafont.kabook.fr/" target="_blank">Contacter Cécile</a>
 
+            <a class="linkteam" href="https://www.linkedin.com/in/webdesign-cecile-lafont/" target="_blank">Contacter Cécile</a>
+            Twitter : @emaginetapub
 
         </div>
     </div>
@@ -86,14 +96,18 @@
             </div>
         </div>
         <h2 class="h2team">            
-            Développeur Back End    
+            Développeur web junior    
         </h2>
         <div class=text-description>
-            Proinde die funestis interrogationibus praestituto imaginarius iudex equitum resedit magister adhibitis aliis iam quae essent agenda praedoctis, et adsistebant hinc inde notarii, quid quaesitum esset, quidve responsum, cursim ad Caesarem perferentes, cuius imperio truci, stimulis reginae exsertantis aurem subinde per aulaeum, nec diluere obiecta permissi nec defensi periere conplures.
+            Issu d'un DUT Métiers du Multimédia et de l’Internet, j'ai décidé d'effectuer la formation Webforce3 pour améliorer mes compétences acquises. Cette formation m'a permis de mieux maîtriser PHP/MySQL ainsi que les techniques d’intégration web.             
             <br><br>
-            Dum apud Persas, ut supra narravimus, perfidia regis motus agitat insperatos, et in eois tractibus bella rediviva consurgunt, anno sexto decimo et eo diutius post Nepotiani exitium, saeviens per urbem aeternam urebat cuncta Bellona, ex primordiis minimis ad clades excita luctuosas, quas obliterasset utinam iuge silentium! ne forte paria quandoque temptentur, plus exemplis generalibus nocitura quam delictis.
+            Curieux, je suis prêt à apprendre des nouvelles technologies (langages de programmation, framework, OS,etc) mais aussi découvrir de nouvelles choses (musique, culture).
             <br><br>
-            <a class="linkteam" href="#" target="_blank">Contacter Landy</a>
+            Actuellement, je suis développeur web pour doob, une société basée sur Bordeaux et spécialisée dans la diffusion de contenu multimédialibre en médiathèque et sur le web.
+            <br><br>
+            <a class="linkteam" href="https://www.linkedin.com/in/landy-razafindrabe-15847b5a" target="_blank">Contacter Landy</a>
+            <br>
+            Twitter : @lrazafindrabe
 
         </div>
     </div>
@@ -108,16 +122,20 @@
             </div>
         </div>
         <h2 class="h2team">            
-            Développeur Front End    
+            Développeur Full Stack    
         </h2>
 
         <div class=text-description>
-            Proinde die funestis interrogationibus praestituto imaginarius iudex equitum resedit magister adhibitis aliis iam quae essent agenda praedoctis, et adsistebant hinc inde notarii, quid quaesitum esset, quidve responsum, cursim ad Caesarem perferentes, cuius imperio truci, stimulis reginae exsertantis aurem subinde per aulaeum, nec diluere obiecta permissi nec defensi periere conplures.
-            <br><br>
-            Dum apud Persas, ut supra narravimus, perfidia regis motus agitat insperatos, et in eois tractibus bella rediviva consurgunt, anno sexto decimo et eo diutius post Nepotiani exitium, saeviens per urbem aeternam urebat cuncta Bellona, ex primordiis minimis ad clades excita luctuosas, quas obliterasset utinam iuge silentium! ne forte paria quandoque temptentur, plus exemplis generalibus nocitura quam delictis.
-            <br><br>
+            Experte en Html5, CSS3, Javascript, Jquery, AngularJS, PHP et mySQL et intermédiaire en Wordpress et responsiveresponsive web design.  J'ai 4 ans d'expérience dans le métier de développeur Web et suite à une formation WebForce3, j'ai toutes les compétences pour être développeur Full Stack.
 
-            <a class="linkteam" href="#" target="_blank">Contacter Jie (Jay)</a>
+            <br><br>
+            Ma prochaine étape : développer sur mobile. Ma meilleure expérience à Shanghai : un site mobile sur une App WECHAT pour une agence immobilière.  
+
+            <br><br>
+            Pour le moment, j'apprends le PHP MVC pour augmenter la qualité du Code.  Vous pouvez me contacter si vous avez besoin de mes services !
+            <br><br>
+            
+            <a class="linkteam" href="https://www.linkedin.com/in/jie-xu-667a4644/" target="_blank">Contacter Jie (Jay)</a>
 
         </div>
     </div>
