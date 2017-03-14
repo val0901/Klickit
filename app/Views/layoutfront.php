3b2ec46cdf814dabe9daa51a8e5e550f922c0f8e
Shifu33 <appeliustheo@gmail.com>
2017-03-14 19:42:54 +0100

    Panier en commentaire

--- app/Views/layoutfront.php
+++ app/Views/layoutfront.php
@@ -174,7 +174,7 @@
 								</div>
 							</div>
 							<div>
-								<a href="<?=$this->url('front_orderList')?>"><button type="button" class="btn btn-primary shoppingmenu_button">VOIR PANIER</button></a>
+								<!-- <a href="<?//=$this->url('front_orderList')?>"><button type="button" class="btn btn-primary shoppingmenu_button">VOIR PANIER</button></a> -->
 							</div>
                         </div>
                     </li>
