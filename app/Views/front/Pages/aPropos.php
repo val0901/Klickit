2cd47dbc10cb469d8303506cfe5bec6004d0484c
cec33 <contact@emaginetapub.com>
2017-03-16 15:05:37 +0100

    correct apropos viewevent

--- app/Views/front/Pages/aPropos.php
+++ app/Views/front/Pages/aPropos.php
@@ -4,7 +4,7 @@
 <div class="apropos_container">
 
     <h1 class="apropos_title">
-    La boutique
+        La boutique
     </h1>
 
     <p class="apropos_text head_apropos"><strong>&copy;Klickit</strong> est une entreprise familiale, née d&#39;un passionné de Playmobil&reg; et d&#39;histoire. Depuis plusieurs années, nous proposons à travers les expositions Playmobils copyright aux quatre coins de la France, de nombreux personnages issus des gammes Playmobil copyright, mais aussi des customs de personnages historiques réalisés par tampographie, créations de pièces en résine, peinture, stickers, pièces détachées, etc... </p>
@@ -21,8 +21,8 @@
 
     <p class="apropos_text">Toutes nos expéditions pour la France sont traitées par La Poste. Les délais d&#39;acheminements sont donc ceux de La Poste : 24 à 48h pour une lettre suivie et 48h pour un colissimo.</p>
 
-     <i class="fa fa-info-circle fa-2x apropospicinfo" aria-hidden="true"></i> <h2 class="apropos_txtinfo">Frais de port :</h2>
-        <!--Frais de port-->
+    <i class="fa fa-info-circle fa-2x apropospicinfo" aria-hidden="true"></i> <h2 class="apropos_txtinfo">Frais de port :</h2>
+    <!--Frais de port-->
     <div class="container_general">
         <div class="orderfrais_border">
             <p class="orderfrais_title">Information frais de port</p>
@@ -38,37 +38,76 @@
     <!--End frais de port-->
     <br><br>
 
-    
-     <i class="fa fa-info-circle fa-2x apropospicinfo" aria-hidden="true"></i> <h2 class="apropos_txtinfo paiement">Paiement</h2>
+
+    <i class="fa fa-info-circle fa-2x apropospicinfo" aria-hidden="true"></i> <h2 class="apropos_txtinfo paiement">Paiement</h2>
     <br><br>
-    <table class="apropos_containpaie">
-        <tr class="apropos_picsortpaie">
-            <td class="apropos_td"><img src="<?=$this->assetUrl('img/logo_paypal_moyens_paiement_fr.png');?>" class="fa fa-cc-paypal fa-4x apropos_sortpaie" aria-hidden="true"></td>
-            <td class="apropos_td"><img src="<?=$this->assetUrl('img/paypal_logo2.jpg');?>" class="fa fa-cc-paypal fa-4x apropos_sortpaie" aria-hidden="true"></td>
-            <td class="apropos_td"><img src="<?=$this->assetUrl('img/paiement_cheque.jpg');?>" class="fa fa-cc-paypal fa-4x apropos_sortpaie" aria-hidden="true"></td>
-            <td class="apropos_td"><img src="<?=$this->assetUrl('img/paiement_virement.jpg');?>" class="fa fa-cc-paypal fa-4x apropos_sortpaie" aria-hidden="true"></td>
-        </tr>
-        <tr class="apropos_sortpaie">
-            <td class="apropos_td">CB</td>
-            <td class="apropos_td">Paypal</td>
-            <td class="apropos_td">Chèque</td>
-            <td class="apropos_td">Virement</td>
-        </tr>
-
-<!--
-        <div class="apropos_sortpaie">
-           
-            Paiement par carte bancaire<br>
-
-            Paypal<br>
-
-            
-            Par chèque (expédition à réception du paiement)<br>
-
-            
-            Par virement bancaire<br>
+    <!-- paypal CB -->
+    <div class="apropos_containpaie">
+        <div class="apropos_picsortpaie">
+            <div class="apropos_td"><img src="<?=$this->assetUrl('img/logo_paypal_moyens_paiement_fr.png');?>" class="fa fa-cc-paypal fa-4x apropos_sortpaie" aria-hidden="true"></div>
+            <div class="apropos_td">CB</div>
         </div>
+
+    <!-- paypal -->
+        <div class="apropos_picsortpaie">
+            <div class="apropos_td"><img src="<?=$this->assetUrl('img/paypal_logo2.jpg');?>" class="fa fa-cc-paypal fa-4x apropos_sortpaie" aria-hidden="true"></div>
+            <div class="apropos_td">Paypal</div>
+        </div>
+
+    <!-- cheque -->
+        <div class="apropos_picsortpaie">
+            <div class="apropos_td"><img src="<?=$this->assetUrl('img/paiement_cheque.jpg');?>" class="fa fa-cc-paypal fa-4x apropos_sortpaie" aria-hidden="true"></div>
+            <div class="apropos_td">Chèque</div>
+        </div>
+
+    <!-- virement -->
+        <div class="apropos_picsortpaie">
+            <div class="apropos_td"><img src="<?=$this->assetUrl('img/paiement_virement.jpg');?>" class="fa fa-cc-paypal fa-4x apropos_sortpaie" aria-hidden="true"></div>
+            <div class="apropos_td">Virement</div>
+        </div>
+
+    </div>
+
+
+
+
+</div>
+
+
+
+<!-- save table
+<table class="apropos_containpaie">
+<tr class="apropos_picsortpaie">
+<td class="apropos_td"><img src="<?=$this->assetUrl('img/logo_paypal_moyens_paiement_fr.png');?>" class="fa fa-cc-paypal fa-4x apropos_sortpaie" aria-hidden="true"></td>
+<td class="apropos_td"><img src="<?=$this->assetUrl('img/paypal_logo2.jpg');?>" class="fa fa-cc-paypal fa-4x apropos_sortpaie" aria-hidden="true"></td>
+<td class="apropos_td"><img src="<?=$this->assetUrl('img/paiement_cheque.jpg');?>" class="fa fa-cc-paypal fa-4x apropos_sortpaie" aria-hidden="true"></td>
+<td class="apropos_td"><img src="<?=$this->assetUrl('img/paiement_virement.jpg');?>" class="fa fa-cc-paypal fa-4x apropos_sortpaie" aria-hidden="true"></td>
+</tr>
+<tr class="apropos_sortpaie">
+<td class="apropos_td">CB</td>
+<td class="apropos_td">Paypal</td>
+<td class="apropos_td">Chèque</td>
+<td class="apropos_td">Virement</td>
+</tr>
+
 -->
-    </table>
+<!--
+<div class="apropos_sortpaie">
+
+Paiement par carte bancaire<br>
+
+Paypal<br>
+
+
+Par chèque (expédition à réception du paiement)<br>
+
+
+Par virement bancaire<br>
+</div>
+-->
+<!--
+</table>
+-->
+
 </div>
 <?php $this->stop('main_content') ?>
