cd2f43dae4e34e17e2f4e7963ef5f7e78b27ab9f
cec33 <contact@emaginetapub.com>
2017-03-21 01:27:19 +0100

    message paynopaypal.php
    facture display non < 470px
    @media print seulement commande

--- app/Views/front/Order/payNoPayPal.php
+++ app/Views/front/Order/payNoPayPal.php
@@ -3,8 +3,26 @@
 <?php $this->start('main_content') ?>
 <div class="container">
 	<div class="row">
-		<div class="col-md-6 ordermidia_width">
-		<p> Merci pour votre commande. Vous pourrez consulter cette facture dans votre <a href="<?=$this->url('front_listOrders')?>" class="hoverlinkorange">historique des commande</a> </p>
+		<div class="col-lg-12 col-md-12 ordermidia_width">
+		<p> Merci pour votre commande. Vous pourrez consulter cette facture dans votre <a href="<?=$this->url('front_listOrders')?>" class="hoverlinkorange">historique de commandes.</a></p><br>
+            <div class="infopay">
+                <p>Vous avez souhaité régler par chèque ou virement. Afin de finaliser votre commande, merci de prendre note de ses informations :</p><br>
+                <p>Pour <strong>régler par chèque</strong>. Merci de le libeller à l'ordre de <strong>"KLICKIT"</strong> et d'inscrire au dos du chèque, le numéro de votre <span style="color:#ef820b;">commande N° <?=$idOrder['id']?></span>.</p>
+                <p>Il sera à adresser à :<br>	
+                <strong>KLICKIT<br>
+                1, résidence beau pré<br>
+                33650 Saucats</strong>
+                </p>
+                <br>
+                <p>Pour <strong>régler par virement bancaire</strong>, merci d'indiquer lors de votre virement bancaire, le numéro de votre <span style="color:#ef820b;">commande N° <?=$idOrder['id']?></span>.</p>
+                <p>Il sera à adresser à :<br>
+                <strong>Laurent Lafont<br>
+                IBAN : FR23 1001 1000 2003 3506 1893 K88<br>
+                BIC : PSSTFRPPCNE</strong>
+                </p>
+                <br>
+                <p class="bg-danger">Nous vous rappelons que le ou les produits vous sont réservés pour une période de 10 jours ouvrés suivant la date de validation de votre commande sur le site. Au delà de cette période, la non réception du règlement entraine l'annulation de la commande.</p>
+            </div>    
 	      <div class="listorder_contenu">
 	          <div>
 	              <div class="row">
@@ -13,7 +31,7 @@
 	                    <p class="baseline_text">Créez l'histoire !</p>
 	                  </div>
 	                  <div class="col-xs-6">
-	                      <p class="viewuserorder_title">FACTURE</p>      
+	                      <p class="viewuserorder_title">COMMANDE</p>      
 	                  </div>      
 	              </div>
 	              <br><br>
@@ -32,13 +50,13 @@
 	                              echo date_format($date, 'd-m-Y');
 	                            ?>   
 	                          </strong></p>
-	                      <p><strong>FACTURE N° <?=$idOrder['id']?></strong></p>
+	                      <p><strong>COMMANDE N° <?=$idOrder['id']?></strong></p>
 	                  </div>
 	              </div>
 	              <br>
 	              <p>N° SIRET : 753 966 464 00012</p>
 	              <br><br>
-	              <p><strong>Facturé à : <?=$user['firstname'].' '.$user['lastname']?></strong></p>
+	              <p><strong>Facturé à : <?=$user['firstname'].'&emsp;'.$user['lastname']?></strong></p>
 	          </div>
 	          
 	          <!--viewuserorder table-->
@@ -97,8 +115,9 @@
 	                <p style="text-align:right;">TVA non applicable, art. 293 B du CGI</p>
 	                <br>
 	                <div>
-	                    <p style="line-height: 10px;">Veuillez libeller votre chèque à l'ordre de Lafont Laurent.</p>
-	                    <p style="line-height: 10px;">Pour toutes questions concernant cette facture, merci de me contacter à <a class="hoverlinkorange" id=" linknav-order" href="<?=$this->url('front_contact');?>">  contact@klickit.fr</a></p>
+	                    <p>Veuillez libeller <strong>votre chèque</strong> à l'ordre de Klickit.</p>
+	                    <p>Veuillez adresser <strong>votre virement</strong> à IBAN : FR23 1001 1000 2003 3506 1893 K88 - BIC : PSSTFRPPCNE.</p>
+	                    <p>Pour toutes questions concernant cette facture, merci de me contacter à <a class="hoverlinkorange" id=" linknav-order" href="<?=$this->url('front_contact');?>">  contact@klickit.fr</a></p>
 	                    <br><br>
 	                    <p style="text-align:center;"><strong>MERCI DE VOTRE CONFIANCE !</strong></p>
 	                </div>
