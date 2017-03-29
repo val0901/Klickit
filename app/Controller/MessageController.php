1e1a07fee91a400ccca3cd6520598dd931651cbc
cec33 <contact@emaginetapub.com>
2017-03-29 20:56:17 +0200

    modif mdp vierge

--- app/Controller/MessageController.php
+++ app/Controller/MessageController.php
@@ -137,6 +137,7 @@ class MessageController extends Controller
 				$sendMail->Host = 'ssl0.ovh.net';  									// Hôte du SMTP
 				$sendMail->SMTPAuth = true;                               				// SMTP Authentification
 				$sendMail->Username = 'contact@klickit.fr'; //Username         				// SMTP username
+				$sendMail->Password = 'mdp'; //mot de passe                    	 				// SMTP password
 				$sendMail->SMTPSecure = 'tls';                         					// Enable TLS encryption, `ssl` also accepted
 				$sendMail->Port = 587;                                					// TCP port to connect to
 				$sendMail->CharSet = 'UTF-8';
