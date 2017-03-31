d51d9a07076a0c53d4bd2d6c11bc98c14e515553
Shifu33 <appeliustheo@gmail.com>
2017-03-31 12:53:12 +0200

    Gestion de stock 2.0

--- app/Controller/FrontOrdersController.php
+++ app/Controller/FrontOrdersController.php
@@ -345,7 +345,7 @@ class FrontOrdersController extends MasterController
 		foreach($content as $key => $value){
 			$stockQuantity = $get->selectStock($value);
 			var_dump($stockQuantity);
-			$stockSoustraction = $get->stockSoustraction($value, $stockQuantity - $quantity[$key]);
+			$stockSoustraction = $get->stockSoustraction($value, $stockQuantity['quantity'] - $quantity[$key]);
 			var_dump($stockSoustraction);
 		}
 
